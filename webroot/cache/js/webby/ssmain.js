ba52d17266ee291936668d09af49b4dbee924773
yoonghan <b_wars@yahoo.com>
2015-07-21 09:21:16 +0800

    Updated target offset based on browser size.

--- webroot/cache/js/webby/ssmain.js
+++ webroot/cache/js/webby/ssmain.js
@@ -265,11 +265,11 @@
 	      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
 	      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
 	       if ($target) {
-	         var targetOffset = $target.offset().top-650;
+	         var targetOffset = $target.offset().top-window.innerHeight-100;
 	         $(this).click(function() {
 	            $("#nav li a").removeClass("active");
 	            $(this).addClass('active');
-	           $('html, body').animate({scrollTop: targetOffset}, 1000);
+	           $('html, body').animate({scrollTop: targetOffset+"px"}, 1000);
 	           return false;
 	         });
 	      }
