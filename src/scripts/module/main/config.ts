d0964f25f6e55a78c73f0286d5a7695676abf341
Han Yoong <b_wars@yahoo.com>
2016-04-09 08:15:18 +0800

    Latest changes

--- src/scripts/module/main/config.ts
+++ src/scripts/module/main/config.ts
@@ -1,8 +1,8 @@
 requirejs.config({
   paths:{
     'scrollToPlugin': '/cache/public/js/gsap/plugins/ScrollToPlugin',
-    'tweenMax': '/cache/public/js/gsap/TweenMax',
-    'timelineMax': '/cache/public/js/gsap/TimelineMax',
+    'tweenMax': '/cache/public/js/gsap/TweenMax.min',
+    'timelineMax': '/cache/public/js/gsap/TimelineMax.min',
     'button':'../button',
     'sectionselect':'../sectionselect',
     'plateselect':'../plateselect'
