931ec0096200cdebf7e2744feaf8ad3f595656d0
Han Yoong <b_wars@yahoo.com>
2017-01-18 22:38:20 +0800

    Added technology page, without image.

--- src/js/components/Technology.tsx
+++ src/js/components/Technology.tsx
@@ -1,29 +1,33 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import { Header } from './Header';
+import { Footer } from "./Footer";
+import { MeTitle } from '../../patternlibrary/js/components/MeTitle';
+import { Gallery } from '../../patternlibrary/js/components/Gallery';
 
-import { Gallery } from "./components/Gallery";
+var styles = require('../../css/components/index');
+declare function require(path: string): any;
 
 const items = [
 	{title: 'Node.js',
 		imgSrc: '/ext/img/technology/nodejs.png',
 		nature:['webserver', 'javascript'], key:'Extremely fast, Non-blocking, Efficient',
-		desc:'I need a fast startup server as the cloud hosting shuts down the application every night. This server serves this purpose, and it is blazing fast as well. Node.js is used along with Express plugin.'},
+		desc:'We needed a fast startup server as the cloud hosting we are using shuts down the application every night. This server serves this purpose, and it is blazing fast as well. NPM is the other key function we relied for development.'},
 	{title: 'React',
     imgSrc: '/ext/img/technology/react.png',
     nature:['library', 'javascript'], key:'Simplicity, Elimination of MVC pattern, Fast',
-    desc:'Similar to AngularJS2, the key features for React is to create modular components in Javascript for reuse. The learning curve for React is steep to grasp, once understood, it is even easier compared to AngularJS.'},
+    desc:'Similar to AngularJS2, the key features for React is to create modular components in Javascript for reuse. The learning curve for React is steep to grasp, once understood, it is easy.'},
 	{title: 'Sass',
     imgSrc: '/ext/img/technology/sass.png',
     nature:['library', 'css'], key:'Familiarity, Readability',
-    desc:'It\'s easy to nest the CSS usages. The codes however are tied closely with PostCSS, which is planned in next migration.'},
+    desc:'Created based on modular CSS. The codes however are tied closely with PostCSS, which are within the planned migration.'},
 	{title: 'Typescript 2.0',
     imgSrc: '/ext/img/technology/typescript.png',
     nature:['compiler', 'javascript'], key:'Type safety check',
-    desc:'Compiler checking detects error faster and helps alot in javascript development. JSX which needs to be compiled in ReactJS benefits alot from this.'},
+    desc:'Compiler checking detects error faster and helps alot in javascript development. JSX which needs to be compiled for ReactJS benefited alot on this.'},
 	{title: 'CommonJS',
 		imgSrc: '/ext/img/technology/commonjs.png',
 		nature:['library', 'javascript'], key:'Eases Library Imports',
-		desc:'Had been using CommonJS as the writing based of all Javascripts. Dropped AMD and SystemJS loading in this development iteration.'},
+		desc:'Had been using CommonJS as the writing based of all Javascripts. Had used this with SystemJS and AMD loads before, now, with react and webpack, a common library is used instead.'},
 	{title: 'React Toolbox',
 		imgSrc: '/ext/img/technology/reacttoolbox.svg',
 		nature:['library', 'javascript', 'plugin'], key:'Material Design Layouts, Simple',
@@ -35,7 +39,7 @@ const items = [
 	{title: 'Webpack',
 		imgSrc: '/ext/img/technology/webpack.png',
 		nature:['compiler', 'build'], key:'Vast plugins and Bundle',
-		desc:'Was using SystemJS (ES6 polyfiller) on the previous iteration, but webpack delves into serious business, hence the usage.'},
+		desc:'Was using SystemJS (ES6 polyfiller) on the previous iteration, but webpack bundles file better. Hopefully ES6 import becomes more available to new browsers.'},
 	{title: 'Gulp',
 		imgSrc: '/ext/img/technology/gulp.png',
 		nature:['compiler', 'build'], key:'Easy, Fast',
@@ -43,7 +47,7 @@ const items = [
 	{title: 'Pug / Jade',
 		imgSrc: '/ext/img/technology/pug.svg',
 		nature:['compiler', 'html'], key:'Easy, Readability',
-		desc:'I loved Jade which is now renamed as Pug. The usage has been trimmed down to minimal due to JSX usage. It\'s still fun to write in Pug.'},
+		desc:'I loved Jade which is now renamed as Pug. The usage has been trimmed down to minimal due to JSX usage. Nevertheless, it\'s still fun to write in Pug.'},
 	{title: 'Animate.css',
 		imgSrc: '/ext/img/technology/npm.png',
 		nature:['css', 'library'], key:'Lightweight, Easy',
@@ -54,7 +58,27 @@ const items = [
 		desc:'An all-ready plugin written in React, that is stable and provides the needed carousel function. Though I\'ve plan to rewrite a custom carousel.'}
 ];
 
-ReactDOM.render(
-    <Gallery items={items}/>,
-    document.getElementById("gallery")
-);
+const metitleInfo  = {
+  title: 'Development Technology',
+  imgSrc: '/ext/img/logo/logoOnlyWhiteBg.svg',
+  introSection: {
+    title: 'Fullstack Development',
+    description: [
+      'These are the technologies used currently for development for this website. '+
+      'We had used many other technologies in our past, but moving forward we do want to try other technologies and tools as every tools created serves a desired purpose. :)'
+    ]
+  }
+}
+
+export class Technology extends React.Component<{}, {}> {
+    render() {
+        return (
+          <div>
+            <Header/>
+            <MeTitle {...metitleInfo}/>
+            <Gallery items={items}/>
+            <Footer/>
+          </div>
+        );
+    }
+}
--- src/patternlibrary/js/components/Gallery.tsx
+++ src/patternlibrary/js/components/Gallery.tsx
@@ -1,6 +1,12 @@
+/**
+*Rotating image background for Overlay Indicator is saved as based 64 in scss
+**/
+
 import * as React from "react";
 import * as ReactDom from "react-dom";
 import * as ReactTransitionGroup from "react-addons-transition-group";
+
+import {Button} from 'react-toolbox/lib/button';
 
 import '../../css/base';
 var styles = require('../../css/components/Gallery');
@@ -10,9 +16,13 @@ interface GalleryState {
   isOverlayOpen: boolean;
   clickPosLeft: number;
   clickPosTop: number;
+  selectedIdx: number;
 }
 
 interface MoziacProps {
+  imgSrc: string;
+  title: string;
+  idx: number;
   clickHandler:any;
 }
 
@@ -20,52 +30,82 @@ interface OverlayProps {
   clickHandler:any;
   animLeft: number;
   animTop: number;
+  items: Array<Item>;
+  selectedIdx: number;
 }
 
 interface OverlayCardProps {
+  clickHandler:any;
   animLeft: number;
   animTop: number;
+  items: Array<Item>;
+  selectedIdx: number;
 }
 
-export class Gallery extends React.Component<{}, GalleryState> {
+interface OverlayCardAnimatedProps {
+  imgSrc: string;
+}
+
+interface OverlayIndicatorProps {
+  total: number;
+  selection: number;
+}
+
+interface GalleryProps {
+  items: Array<Item>;
+}
+
+export interface Item {
+  title: string;
+  imgSrc: string;
+  nature: Array<string>;
+  key: string;
+  desc: string;
+}
+
+export class Gallery extends React.Component<GalleryProps, GalleryState> {
   constructor(props:any) {
     super(props);
     this.state = {
       isOverlayOpen: false,
       clickPosLeft: 0,
-      clickPosTop: 0
+      clickPosTop: 0,
+      selectedIdx: 0
     };
   };
 
-  toggleOverlay = (posTop:number = 0, posLeft:number = 0) => {
+  createMoziacListing = ():JSX.Element[] => {
+    const moziacListing = this.props.items;
+
+    let idx = 0;
+    return moziacListing.map(
+      (moziac) =>{
+        return (
+          <Moziac clickHandler={this.toggleOverlay} imgSrc={moziac.imgSrc} title={moziac.title} key={moziac.title} idx={idx++}></Moziac>
+        );
+      }
+    );
+  }
+
+  toggleOverlay = (posTop:number = 0, posLeft:number = 0, selectedIdx:number = 0) => {
     const {isOverlayOpen} = this.state;
 
     this.setState({
       isOverlayOpen: !isOverlayOpen,
       clickPosLeft: posLeft,
-      clickPosTop: posTop
+      clickPosTop: posTop,
+      selectedIdx: selectedIdx
     });
   };
 
   render() {
     return (
       <div className={styles.gallery}>
-
         <ReactTransitionGroup>
-            {this.state.isOverlayOpen && <Overlay clickHandler={this.toggleOverlay} animTop={this.state.clickPosTop} animLeft={this.state.clickPosLeft}/>}
+            {this.state.isOverlayOpen &&
+              <Overlay clickHandler={this.toggleOverlay} animTop={this.state.clickPosTop} animLeft={this.state.clickPosLeft} items={this.props.items} selectedIdx={this.state.selectedIdx}/>}
         </ReactTransitionGroup>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
-        <Moziac clickHandler={this.toggleOverlay}></Moziac>
+        {this.createMoziacListing()}
       </div>
     );
   }
@@ -74,79 +114,160 @@ export class Gallery extends React.Component<{}, GalleryState> {
 class Overlay extends React.Component<OverlayProps, {}> {
 
   static leaveTime = 100;
-  static heightAllow = 480;
-
-  componentDidMount () {
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-
-    console.log("??"+windowHeight + "," + Overlay.heightAllow);
-    if(windowHeight > Overlay.heightAllow) {
-      document.body.style.overflow = 'hidden';
-    }
-  }
-
-  componentWillUnmount () {
-    document.body.style.overflow = '';
-  }
 
   handleClickHandler = () => {
     this.props.clickHandler();
   };
 
   componentWillEnter (callback:any) {
-    const el = ReactDom.findDOMNode(this);
-    el.classList.add(styles['overlay-enter']);
     setTimeout(function(){callback();}, Overlay.leaveTime);
-  }
+  };
 
   componentDidEnter() {
     const el = ReactDom.findDOMNode(this);
     el.classList.add(styles['overlay-enter-active']);
-  }
+  };
 
   componentWillLeave (callback:any) {
     const el = ReactDom.findDOMNode(this);
     el.classList.add(styles['overlay-leave-active']);
     setTimeout(function(){callback();}, Overlay.leaveTime);
-  }
+  };
 
   render() {
-    const {animLeft, animTop} = this.props;
+    const {animLeft, animTop, items, selectedIdx} = this.props;
     return (
       <div className={styles.overlay}>
         <div className={styles['overlay-background']} onClick={this.handleClickHandler}></div>
-        <OverlayCard animLeft={animLeft} animTop={animTop}/>
+        <OverlayCard animLeft={animLeft} animTop={animTop} clickHandler={this.handleClickHandler} items={items} selectedIdx={selectedIdx}/>
       </div>
     );
   }
 }
 
 class OverlayCard extends React.Component<OverlayCardProps, {}> {
+  handleClickHandler = () => {
+    this.props.clickHandler();
+  };
+
   componentDidMount () {
     const el = ReactDom.findDOMNode(this) as HTMLElement;
+    const heightAllow = el.getBoundingClientRect().height + 10;
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    if(windowHeight > heightAllow) {
+      document.body.style.overflow = 'hidden';
+    }
+
     setTimeout(function(){
       const windowTopPos = window.pageYOffset || document.documentElement.scrollTop;
-      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
       const docHeight = document.body.clientHeight;
-      if(windowTopPos < (docHeight - Overlay.heightAllow)) {
-        el.style.top = (windowTopPos + 10) + "px";
+      const scrollableHeight = (docHeight - heightAllow) > 0 ? (docHeight - heightAllow) : 0;
+      if(windowTopPos < scrollableHeight) {
+        let allowedHeight = windowHeight - heightAllow;
+        el.style.top = (windowTopPos + (allowedHeight>0 ? allowedHeight/2 : 0)) + "px";
       }
       else {
-        window.scrollTo( 0, 0);
-        el.style.top = "10px";
+        window.scrollTo( 0, scrollableHeight);
+        el.style.top = "";
+        el.style.bottom = "0";
       }
-      el.style.minHeight = Overlay.heightAllow + 'px';
+      el.style.minHeight = heightAllow + 'px';
       el.style.left = "50%";
     },
     Overlay.leaveTime);
   }
 
+
+  componentWillUnmount () {
+    document.body.style.overflow = '';
+  }
+
+  createKeyFeatures = () => {
+    const {nature} = this.props.items[this.props.selectedIdx];
+
+    return nature.map(
+      (natureItem) => {
+        return <div key={natureItem}><i className="fa fa-check-circle-o" aria-hidden="true"></i> {natureItem}</div>
+      }
+    );
+
+  }
+
   render() {
-    const {animLeft, animTop} = this.props;
+    const {animLeft, animTop, items, selectedIdx} = this.props;
+    const {title, imgSrc, nature, key, desc} = items[selectedIdx];
+
     const animateStartPos = (animLeft && animTop) ? {top: animTop, left:animLeft} : {};
     return (
-      <div className={styles.overlaycard} style={animateStartPos}></div>
+      <div className={styles.overlaycard} style={animateStartPos}>
+        <div className={styles['overlaycard-bg']}>
+          <OverlayIndicator total={items.length} selection={selectedIdx}/>
+        </div>
+        <div className={styles['overlaycard-image']}>
+          <div className={styles['overlaycard-image-adj']}>
+            <OverlayCardImageAnimated imgSrc={imgSrc}/>
+          </div>
+        </div>
+        <div className={styles['overlaycard-placement']}>
+          <div className={styles['overlaycard-content']}>
+            <h2>{title}</h2>
+            <div className={styles['overlaycard-content-features']}>
+              {this.createKeyFeatures()}
+            </div>
+            <hr/>
+            <div className={styles['overlaycard-content-keyfeature']}>
+              <h5>Key Features</h5>
+              <span>{key}</span>
+            </div>
+            <hr/>
+            <div className={styles['overlaycard-content-desc']}>
+              <h5>Description</h5>
+              <span>{desc}</span>
+            </div>
+            <hr/>
+          </div>
+          <div className={styles['overlaycard-button']}>
+            <Button icon='close' floating accent mini onClick={this.handleClickHandler}/>
+          </div>
+        </div>
+      </div>
     );
+  }
+}
+
+class OverlayIndicator extends React.Component<OverlayIndicatorProps, {}> {
+
+  static imageAngleStart = 90;
+
+  render() {
+    const {total, selection} = this.props;
+    const angle = (Math.floor(180 / (total - 1)) * selection) - OverlayIndicator.imageAngleStart;
+    const rotation = {transform: 'rotate(' + angle + 'deg)'};
+
+    return (
+      <div className={styles['overlaycard-bg-indicator']} style={rotation}></div>
+    )
+  }
+}
+
+class OverlayCardImageAnimated extends React.Component<OverlayCardAnimatedProps, {}> {
+  static animation="jello";
+
+  componentDidMount () {
+    const el = ReactDom.findDOMNode(this);
+    setTimeout(function(){
+      el.classList.add(OverlayCardImageAnimated.animation);
+    },
+    Overlay.leaveTime);
+  }
+
+  render() {
+    const {imgSrc} = this.props;
+
+    return (
+      <img src={imgSrc} className="animated"/>
+    )
   }
 }
 
@@ -155,14 +276,15 @@ class Moziac extends React.Component<MoziacProps, {}> {
     const el = ReactDom.findDOMNode(this);
     const posTop = el.getBoundingClientRect().top;
     const posLeft = el.getBoundingClientRect().left;
-    this.props.clickHandler(posTop, posLeft);
+    this.props.clickHandler(posTop, posLeft, this.props.idx);
   };
 
   render() {
+    const {title, imgSrc} = this.props;
     return (
       <div className={styles.moziac} onClick={this.handleClickHandler}>
-        <img className={styles['moziac-image']} src="/ext/img/about/gladys.jpg"></img>
-        <div className={styles['moziac-title']}>React JS</div>
+        <img className={styles['moziac-image']} src={imgSrc}></img>
+        <div className={styles['moziac-title']}>{title}</div>
       </div>
     );
   }
