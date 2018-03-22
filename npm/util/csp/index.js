ac20f2dfabcae14c125d8b073b7de23876f4ee88
Han <han.yoong@daimler.com>
2018-03-22 23:11:02 +0800

    Security filter

--- npm/util/csp/index.js
+++ npm/util/csp/index.js
@@ -3,10 +3,14 @@
  */
 module.exports =  {
   directives: {
-    defaultSrc: ["'self'"],
+    defaultSrc: ["'none'"],
     scriptSrc: ["'self'", "'unsafe-inline'"],
     styleSrc: ["'self'", 'fonts.googleapis.com', 'blob:', "'sha256-DzBaz0CFnwWcMZJGgBdaBSm526NuvqFR+6vjX2QW/Wc='", "'sha256-bWLnx2krAUW7we0zPmPs09KbYBg7SeMgAq3nWzw23yE='"],
     fontSrc: ["'self'", 'fonts.gstatic.com'],
+    imgSrc: ["'self'"],
+    frameAncestors: ["'none'"],
+    baseUri: ["'self'"],
+    formAction: ["'self'"],
     workerSrc: false
   }
 };
