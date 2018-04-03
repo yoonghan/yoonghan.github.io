954d11610b659bea0cd23cff029341c488b980b5
Han <han.yoong@daimler.com>
2018-04-04 07:04:29 +0800

    Commit for IE fixes

--- npm/util/csp/index.js
+++ npm/util/csp/index.js
@@ -18,7 +18,7 @@ module.exports = function generateCsp(testEnvUrl) {
       connectSrc: _connectSrc,
       styleSrc: ["'self'", 'fonts.googleapis.com', 'blob:', "'sha256-IxBqGB25PsRAAJRiQR4+dYowUtzFBFXo9t3ERhZPhWI='", "'sha256-bWLnx2krAUW7we0zPmPs09KbYBg7SeMgAq3nWzw23yE='", "'sha256-9cv2aWOtBuARDX4PnzHn4/KqxpqXvz1JP5z8MV7rDvY='"],
       fontSrc: ["'self'", 'fonts.gstatic.com'],
-      imgSrc: ["'self'"],
+      imgSrc: ["'self'", "data:"],
       frameAncestors: ["'none'"],
       baseUri: ["'self'"],
       formAction: ["'self'"],
