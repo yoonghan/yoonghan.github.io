71ce753394914314bcb5a4beb3dd1e40906481ef
Walcron <b_wars@yahoo.com>
2019-08-10 17:10:19 +0800

    Fix body style

--- components/html/HtmlHead.tsx
+++ components/html/HtmlHead.tsx
@@ -11,22 +11,24 @@ interface HtmlHeadProps {
 
 export const HtmlHead: React.SFC<HtmlHeadProps> = ({title, description}) => {
   return (
-    <Head>
-      <meta charSet="utf-8" key="charset"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport"/>
-      <meta name="og:title" content="Walcron Coorperation" key="fb_title"/>
-      <meta name="og:type" content="profile" key="fb_type"/>
-      <meta name="og:site_name" content="http://www.walcron.com/" key="fb_charset"/>
-      <meta property="og:image:type" content="image/png" key="fb_image_t"/>
-      <meta property="og:image:width" content="400" key="fb_image_w"/>
-      <meta property="og:image:height" content="400" key="fb_image_h"/>
-      <meta name="og:image" content="http://www.walcron.com/og_image.png" key="fb_image_i"/>
-      <meta name="og:description" content="Welcome to Walcron Coorperation, Malaysia; a site was built for IT research purposes." key="fb_image_desc"/>
-      <meta name="fb:admins" content="walcoorperation@gmail.com" key="fb_image_m"/>
-      <title>{title}</title>
-      <meta name="description" content={description}/>
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" key="font"></link>
-      <link rel="stylesheet" href="/css/font.css" key="internalfont"></link>
+    <React.Fragment>
+      <Head>
+        <meta charSet="utf-8" key="charset"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport"/>
+        <meta name="og:title" content="Walcron Coorperation" key="fb_title"/>
+        <meta name="og:type" content="profile" key="fb_type"/>
+        <meta name="og:site_name" content="http://www.walcron.com/" key="fb_charset"/>
+        <meta property="og:image:type" content="image/png" key="fb_image_t"/>
+        <meta property="og:image:width" content="400" key="fb_image_w"/>
+        <meta property="og:image:height" content="400" key="fb_image_h"/>
+        <meta name="og:image" content="http://www.walcron.com/og_image.png" key="fb_image_i"/>
+        <meta name="og:description" content="Welcome to Walcron Coorperation, Malaysia; a site was built for IT research purposes." key="fb_image_desc"/>
+        <meta name="fb:admins" content="walcoorperation@gmail.com" key="fb_image_m"/>
+        <title>{title}</title>
+        <meta name="description" content={description}/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" key="font"></link>
+        <link rel="stylesheet" href="/css/font.css" key="internalfont"></link>
+      </Head>
       <style jsx global>
       {`
         html {
@@ -42,6 +44,6 @@ export const HtmlHead: React.SFC<HtmlHeadProps> = ({title, description}) => {
         }
       `}
       </style>
-    </Head>
+    </React.Fragment>
   );
 }
