93362e084f6a2adb78db2ffece2237c0208c1984
Walcron <b_wars@yahoo.com>
2019-08-18 00:36:03 +0800

    Latest release

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express'),
       hsts = require('hsts'),
       next = require('next');
 
-const hostApp = require('./hostApp');
-
 const dev = process.env.NODE_ENV !== 'production';
 const port = parseInt(process.env.PORT, 10) || 8001;
 const app = next({ dev });
@@ -23,45 +21,11 @@ app.prepare()
   }));
   /** [Security-End] **/
 
-  /** [SEO-Start] **/
-  const seoOptions = {
-    root: __dirname + '/seo/',
-    headers: {
-      'Content-Type': 'text/plain;charset=UTF-8',
-    }
-  };
-  server.get('/robots.txt', (req, res) => (
-    res.status(200).sendFile('robots.txt', seoOptions)
-  ));
-  server.get('/sitemap.xml', (req, res) => {
-    seoOptions["headers"] = {"Content-Type": "application/xhtml+xml;charset=UTF-8"}
-    return res.status(200).sendFile('sitemap.xml', seoOptions)
-  });
-  server.get('/og_image.png', (req, res) => {
-    seoOptions["headers"] = {"Content-Type": "image/png"}
-    return res.status(200).sendFile('og_image.png', seoOptions)
-  });
-  server.get('/favicon.ico', (req, res) => {
-    seoOptions["headers"] = {"Content-Type": "image/x-icon"}
-    return res.status(200).sendFile('favicon.ico', seoOptions)
-  });
-  /** [SEO-End] **/
-
   /** [Static-Start] **/
   server.use(express.static('./static', {
     maxage: '24h'
   }));
   /** [Static-End] **/
-
-  /** [SiteHosting-Start] **/
-  server.use('/host', express.static('./host', {
-    maxage: '24h'
-  }));
-  /** [SiteHosting-End] **/
-
-  /** [Allow External Hosting-Start] **/
-  hostApp(server);
-  /** [Allow External Hosting-End]**/
 
   /** [Request-Start]**/
   server.get('*', (req, res) => {
