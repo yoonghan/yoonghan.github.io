2137b575ab6d6013f9e708663990b78645ff0d23
han.yoong <b_wars@yahoo.com>
2019-12-05 18:25:34 +0800

    Added service worker

--- next.config.js
+++ next.config.js
@@ -1,8 +1,35 @@
+const withOffline = require('next-offline');
+const ServiceWorkerPlugin = require('service-worker-plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-const configuration = withBundleAnalyzer({
+const configuration = withOffline({
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  generateInDevMode: false,
+  workboxOpts: {
+  swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.node = {
