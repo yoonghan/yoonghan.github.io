db3c926a917aeadb55d51e7e0f693ec5a985b518
han.yoong <b_wars@yahoo.com>
2019-12-12 18:32:16 +0800

    Add chat message box.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,21 +2,31 @@ const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve("babel-loader")
-      },
-      {
-        loader: require.resolve("awesome-typescript-loader")
-      },
-      {
-        loader: require.resolve("react-docgen-typescript-loader")
-      }
-    ]
-  });
-  config.resolve.extensions.push(".ts", ".tsx");
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: require.resolve("babel-loader")
+        },
+        {
+          loader: require.resolve("awesome-typescript-loader")
+        },
+        {
+          loader: require.resolve("react-docgen-typescript-loader")
+        }
+      ]
+    }
+  );
+  config.module.rules.push(
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }
+  );
+  config.resolve.extensions.push(".js", ".jsx", ".ts", ".tsx");
   /**Resolves to not use tslint, awesome typescript sends error immediately, forkts relies on linting.**/
   // config.plugins.push(
   //   new ForkTsCheckerWebpackPlugin({
