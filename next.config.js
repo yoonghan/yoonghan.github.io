a89d58322ff3c4678417a204e5778edae6ef3272
han.yoong <b_wars@yahoo.com>
2019-12-12 17:29:11 +0800

    Reverted - issues are zeit comes with typescript built in. Real issue is that memory cannot be increased, revert to JS rather than TS/TSX files.

--- next.config.js
+++ next.config.js
@@ -1,57 +1,54 @@
-const withTypescript = require('@zeit/next-typescript');
 const withOffline = require('next-offline');
 const ServiceWorkerPlugin = require('service-worker-plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const configuration = withTypescript(
-  withBundleAnalyzer(
-    withOffline(
-      {
-        dontAutoRegisterSw: true,
-        target: 'serverless',
-        transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
-        // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-        // turn on the SW in dev mode so that we can actually test it
-        generateInDevMode: false,
-        workboxOpts: {
-        swDest: 'static/service-worker.js',
-          runtimeCaching: [
-            {
-              urlPattern: /^https?.*/,
-              handler: 'NetworkFirst',
-              options: {
-                cacheName: 'https-calls',
-                networkTimeoutSeconds: 15,
-                expiration: {
-                  maxEntries: 150,
-                  maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-                },
-                cacheableResponse: {
-                  statuses: [0, 200],
-                },
+const configuration = withBundleAnalyzer(
+  withOffline(
+    {
+      dontAutoRegisterSw: true,
+      target: 'serverless',
+      transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+      // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+      // turn on the SW in dev mode so that we can actually test it
+      generateInDevMode: false,
+      workboxOpts: {
+      swDest: 'static/service-worker.js',
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'https-calls',
+              networkTimeoutSeconds: 15,
+              expiration: {
+                maxEntries: 150,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
               },
             },
-          ],
-        },
-        webpack: (config, { isServer }) => {
-          if (!isServer) {
-            config.node = {
-              fs: 'empty',
-              module: 'empty'
-            }
+          },
+        ],
+      },
+      webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.node = {
+            fs: 'empty',
+            module: 'empty'
           }
-          return config;
-        },
-        env: {
-          PUSHER_APP_ID: process.env.PUSHER_APP_ID||"PUSHER_APP_ID",
-          PUSHER_APP_KEY: process.env.PUSHER_APP_KEY||"PUSHER_APP_KEY",
-          PUSHER_SECRET: process.env.PUSHER_SECRET||"PUSHER_SECRET",
-          PUSHER_CLUSTER: process.env.PUSHER_CLUSTER||"PUSHER_CLUSTER"
         }
+        return config;
+      },
+      env: {
+        PUSHER_APP_ID: process.env.PUSHER_APP_ID||"PUSHER_APP_ID",
+        PUSHER_APP_KEY: process.env.PUSHER_APP_KEY||"PUSHER_APP_KEY",
+        PUSHER_SECRET: process.env.PUSHER_SECRET||"PUSHER_SECRET",
+        PUSHER_CLUSTER: process.env.PUSHER_CLUSTER||"PUSHER_CLUSTER"
       }
-    )
+    }
   )
 );
 
