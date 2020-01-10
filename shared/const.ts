034345308d36178843b0293b4831df71d7354443
han.yoong <b_wars@yahoo.com>
2020-01-10 14:43:36 +0800

    Added channel name authorization to prevent eavesdrop on expired connection

--- shared/const.ts
+++ shared/const.ts
@@ -1,6 +1,7 @@
 export const EMAIL="w_a_l_c_o_o_r_p_e_r_a_t_i_o_n_@_g_m_a_i_l_._c_o_m";
 export const PUSHER = {
   event: "walcron_evt_push",
-  channel: "walcron_c",
+  channel_prefix: "wal_",
+  //channel: "walcron_c",   //Deprecated - Channel is based on token returned.
   endpoint: "/api/pusherauth"
 }
