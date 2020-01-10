891cec230b1b389eeae9b48dab35413e2f625ccd
Walcron <b_wars@yahoo.com>
2020-01-10 21:18:58 +0800

    Resolve issue of getting correct token api/codegen

--- next.config.js
+++ next.config.js
@@ -46,7 +46,8 @@ const configuration = withBundleAnalyzer(
         PUSHER_APP_ID: process.env.PUSHER_APP_ID||"PUSHER_APP_ID",
         PUSHER_APP_KEY: process.env.PUSHER_APP_KEY||"PUSHER_APP_KEY",
         PUSHER_SECRET: process.env.PUSHER_SECRET||"PUSHER_SECRET",
-        PUSHER_CLUSTER: process.env.PUSHER_CLUSTER||"PUSHER_CLUSTER"
+        PUSHER_CLUSTER: process.env.PUSHER_CLUSTER||"PUSHER_CLUSTER",
+        API_CALL: process.env.API_CALL||"https://www.walcron.com"
       }
     }
   )
