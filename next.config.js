983ed26d60a3d0d1f26b4cd8a12f83e39cf29925
Walcron <b_wars@yahoo.com>
2020-01-11 19:31:59 +0800

    Remove unwanted keys

--- next.config.js
+++ next.config.js
@@ -43,9 +43,7 @@ const configuration = withBundleAnalyzer(
         return config;
       },
       env: {
-        PUSHER_APP_ID: process.env.PUSHER_APP_ID,
         PUSHER_APP_KEY: process.env.PUSHER_APP_KEY,
-        PUSHER_SECRET: process.env.PUSHER_SECRET,
         PUSHER_CLUSTER: process.env.PUSHER_CLUSTER
       }
     }
