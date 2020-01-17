890ae997b4db938ede213e443e5a58083e712468
han.yoong <b_wars@yahoo.com>
2020-01-17 16:47:32 +0800

    Quicken load time

--- components/HtmlHead/index.tsx
+++ components/HtmlHead/index.tsx
@@ -29,7 +29,7 @@ export class HtmlHead extends React.Component<HtmlHeadProps, {}> {
   }
 
   componentDidMount() {
-    setTimeout(this._runPreload, 100);
+    setTimeout(this._runPreload, 50);
   }
 
   render() {
