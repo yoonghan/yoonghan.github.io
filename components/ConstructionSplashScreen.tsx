5cea862613280e77f8cc4fd806a60b044a730341
Walcron <b_wars@yahoo.com>
2020-01-25 23:29:03 +0800

    Improve error page display.

--- components/ConstructionSplashScreen.tsx
+++ components/ConstructionSplashScreen.tsx
@@ -1,6 +1,7 @@
 `use strict`
 
 import * as React from "react";
+import Button from "./Button";
 import {fontColor, background} from "../shared/style";
 
 const ConstructionSplashScreen: React.SFC<any> = () => {
@@ -10,24 +11,31 @@ const ConstructionSplashScreen: React.SFC<any> = () => {
         <div className={"banner"}>(404) NOT FOUND</div>
         <img src="/static/img/construction.svg"/>
         <hr/>
-        <div className={"footnote"}>Walcron ver 5.0</div>
+        <div className={"buttonContainer"}>
+          <Button href="/">Return as page is not found</Button>
+        </div>
       </div>
       <style jsx>{`
-
+        .buttonContainer {
+          margin-left: auto;
+          margin-right: auto;
+          text-align: center;
+        }
+        .buttonContainer :global(a) {
+          background-color: #000;
+          color: #FFF;
+        }
         .banner {
           text-align: center;
           font-size: 2rem;
           font-family: fantasy, arial, sans-serif;
           padding-bottom: 5px;
-
         }
-
         .footnote {
           font-size: 1rem;
           text-align: center;
           font-family: arial, sans-serif;
         }
-
         img {
           display: block;
           margin-left: auto;
@@ -36,18 +44,15 @@ const ConstructionSplashScreen: React.SFC<any> = () => {
           opacity: 0.3;
           max-height: 320px;
         }
-
         h5 {
           margin: 10px 0 0 0;
           font-weight: normal;
           font-family: arial;
         }
-
         hr {
           margin: 20px 0;
           border-top: 1px solid #90A4AE;
         }
-
         .construction-splash-screen-container {
           position: relative;
           height: 100vh;
@@ -55,20 +60,17 @@ const ConstructionSplashScreen: React.SFC<any> = () => {
           background: ${background['construction']};
           color: #000;
         }
-
         .construction-splash-screen-text-container {
           position: absolute;
           top: 50%;
           transform: translate(0, -50%);
           width: 100%;
         }
-
         .construction-splash-screen-container h1 {
           font-size: 5rem;
           color: ${fontColor['construction-h1']};
           text-align: center;
         }
-
         .construction-splash-screen-container h5 {
           font-size: 1rem;
           color: ${fontColor['construction-h5']};
