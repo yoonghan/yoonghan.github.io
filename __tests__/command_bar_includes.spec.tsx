7b9de086f8c147e3d2ebfc00b46a75c18fd6c4ad
Walcron <b_wars@yahoo.com>
2020-01-25 23:28:30 +0800

    Update testing script

--- __tests__/command_bar_includes.spec.tsx
+++ __tests__/command_bar_includes.spec.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import {mount} from 'enzyme';
+import { mount} from 'enzyme';
 import IndexPage from '../pages/index';
 import AboutPage from '../pages/about';
 import CreationPage from '../pages/creation';
 import TributePage from '../pages/tribute';
 import ManipulatorPage from '../pages/manipulator';
+
+//Increase wait time
+jest.setTimeout(30000);
 
 //Rewrite router.
 jest.mock("next/router");
@@ -12,35 +15,52 @@ jest.mock("next/router");
 //Rewrite dynamic.
 jest.mock('next/dynamic');
 
+const waitNextTick = async () => {
+  return new Promise(resolve => setTimeout(resolve, 2000));
+}
+
+const mountInitialComponent = async (component: any) => {
+  const wrapper = mount(component);
+  await waitNextTick();
+  wrapper.update();
+  return wrapper;
+};
+
 describe('Pages', () => {
   describe('Index', () => {
-    it('should contain commandbar', function () {
-      const wrap = mount(<IndexPage termsRead={"false"}/>);
+    it('should contain commandbar', async () => {
+      const wrap = await mountInitialComponent(<IndexPage termsRead={"false"}/>);
+      expect(wrap.find('#commandbar').exists()).toBe(true);
+    })
+  }),
+  describe('Index', () => {
+    it('should contain commandbar', async () => {
+      const wrap = await mountInitialComponent(<IndexPage termsRead={"false"}/>);
       expect(wrap.find('#commandbar').exists()).toBe(true);
     })
   }),
   describe('About', () => {
-    it('should contain commandbar', function () {
-      const wrap = mount(<AboutPage/>);
+    it('should contain commandbar', async () => {
+      const wrap = await mountInitialComponent(<AboutPage/>);
       expect(wrap.find('#commandbar').exists()).toBe(true);
     })
   }),
   describe('Creation', () => {
-    it('should contain commandbar', function () {
-      const wrap = mount(<CreationPage/>);
+    it('should contain commandbar', async () => {
+      const wrap = await mountInitialComponent(<CreationPage/>);
       expect(wrap.find('#commandbar').exists()).toBe(true);
     })
   }),
   describe('Tribute', () => {
-    it('should contain commandbar', function () {
-      const wrap = mount(<TributePage/>);
+    it('should contain commandbar', async () => {
+      const wrap = await mountInitialComponent(<TributePage/>);
       expect(wrap.find('#commandbar').exists()).toBe(true);
     })
   }),
   describe('Manipulator', () => {
-    it('should contain commandbar', function () {
+    it('should contain commandbar', async () => {
       /** Will throw exception, i.e. wrapped not called. **/
-      const wrap = mount(<ManipulatorPage/>);
+      const wrap = await mountInitialComponent(<ManipulatorPage/>);
       expect(wrap.find('#commandbar').exists()).toBe(true);
     })
   })
