f53005a4b8562b1be6d8155e6aa4fadc1e81de37
Walcron <b_wars@yahoo.com>
2020-02-02 14:19:55 +0800

    Update Component test.

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -2,94 +2,78 @@
 
 import * as React from "react";
 import NoSSR from 'react-no-ssr';
-import produce, {Draft} from "immer";
 import {LINK} from "../shared/style";
 
 export interface ScrollToTopStates {
   visible: boolean;
 }
 
-export interface ScrollToTopProps {
+const _isOverTheBar = () => {
+  const currentScrollPos = window.pageYOffset;
+  return currentScrollPos > 320;
 }
 
-class ScrollToTop extends React.PureComponent<ScrollToTopProps, ScrollToTopStates> {
-  constructor(props:ScrollToTopProps) {
-    super(props);
-    this.state = {
-      visible: this._isOverTheBar()
+const ScrollToTop = () => {
+  const [visible, setVisible] = React.useState(_isOverTheBar());
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", _handleScroll);
+    return () => {
+      window.removeEventListener("scroll", _handleScroll);
     };
-  }
+  },[]);
 
-  componentDidMount() {
-    window.addEventListener("scroll", this._handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this._handleScroll);
-  }
-
-  _isOverTheBar = () => {
-    const currentScrollPos = window.pageYOffset;
-    return currentScrollPos > 320;
-  }
-
-  _handleScroll = () => {
-    this.setState(
-      produce((draft: Draft<ScrollToTopStates>) => {
-        draft.visible = this._isOverTheBar()
-      })
-    );
+  const _handleScroll = () => {
+    setVisible(_isOverTheBar())
   };
 
-  _clickScrollUp = () => {
+  const _clickScrollUp = () => {
     window.scrollTo(0, 0);
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.state.visible && <div onClick={this._clickScrollUp}>Up</div>}
-        <style jsx>{`
+  return (
+    <React.Fragment>
+      {visible && <div onClick={_clickScrollUp}>Up</div>}
+      <style jsx>{`
+        div {
+          right: 2rem;
+          position: fixed;
+          bottom: 2rem;
+          cursor: pointer;
+          background: ${LINK.FOREGROUND};
+          color: ${LINK.BACKGROUND};
+          opacity: 0.7;
+          padding: 5px;
+          border-radius: 3px;
+        }
+
+        div:before {
+          content: '';
+          position: absolute;
+          top: -5px;
+          left: 0px;
+          margin-left: 10px;
+          width: 0;
+          height: 0;
+          border-bottom: solid 5px #FFF;
+          border-left: solid 5px transparent;
+          border-right: solid 5px transparent;
+        }
+
+        @media only screen and (max-width: 480px) {
           div {
-            right: 2rem;
-            position: fixed;
-            bottom: 2rem;
-            cursor: pointer;
-            background: ${LINK.FOREGROUND};
-            color: ${LINK.BACKGROUND};
-            opacity: 0.7;
-            padding: 5px;
-            border-radius: 3px;
+            bottom: 0;
+            right: 0;
+            padding: 5px 20px;
+            border-radius: 0;
           }
-
           div:before {
-            content: '';
-            position: absolute;
-            top: -5px;
-            left: 0px;
-            margin-left: 10px;
-            width: 0;
-            height: 0;
-            border-bottom: solid 5px #FFF;
-            border-left: solid 5px transparent;
-            border-right: solid 5px transparent;
+            margin-left: 25px;
           }
-
-          @media only screen and (max-width: 480px) {
-            div {
-              bottom: 0;
-              right: 0;
-              padding: 5px 20px;
-              border-radius: 0;
-            }
-            div:before {
-              margin-left: 25px;
-            }
-          }
-        `}</style>
-      </React.Fragment>
-    );
-  }
+        }
+      `}</style>
+    </React.Fragment>
+  );
 }
 
 const ScrollToTopWithNoSSR = () => (
