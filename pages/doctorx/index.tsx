b0cdea045f942caf5d2f7025f6b6c52b095fd4e5
Walcron <b_wars@yahoo.com>
2020-03-29 21:03:20 +0800

    For web reading from props.

--- pages/doctorx/index.tsx
+++ pages/doctorx/index.tsx
@@ -17,10 +17,22 @@ const printSystem = (setDisplayState:any) => (msg:string) => {
   }
 }
 
-const printEvent = (setDisplayState:any) => (msg:string) => {
-  setDisplayState(EnumDisplayState.PRINTABLE);
+const printEvent = (setDisplayState:any, setInfo:any) => (msg:string) => {
   const info = JSON.parse(msg);
-  console.log(info);
+  setDisplayState(EnumDisplayState.PRINTABLE);
+  setInfo(info);
+}
+
+const drawResult = (info: object) => {
+  const infos = [];
+  for (const property in info) {
+    infos.push(<div>{property}: {info[property]}</div>);
+  }
+  return infos;
+}
+
+const print = () => {
+  window.print();
 }
 
 interface DoctorxProps {
@@ -31,12 +43,13 @@ const Doctorx:any = (props: DoctorxProps) => {
   const {messengerApi} = props;
   const canvasref = React.useRef(null);
   const [displayState, setDisplayState] = React.useState(EnumDisplayState.QRCODE);
+  const [info, setInfo] = React.useState({});
   const toFillUrl = "https://www.walcron.com/doctorx/formfill";
   const _printSystem = React.useMemo(
     ()=>printSystem(setDisplayState)
     ,[displayState]);;
   const _printEvent = React.useMemo(
-    ()=>printEvent(setDisplayState)
+    ()=>printEvent(setDisplayState, setInfo)
     ,[displayState]);
 
   React.useEffect(()=>{
@@ -51,11 +64,18 @@ const Doctorx:any = (props: DoctorxProps) => {
   const _displayBody = () => {
     switch(displayState) {
       case EnumDisplayState.QRCODE:
-        return <canvas ref={canvasref} className="container"/>;
+        return <div style={{textAlign:"center", padding: "40px"}}><canvas ref={canvasref} className="container"/></div>
       case EnumDisplayState.CLOSED:
         return <p>Closed due to non activity, to restore refresh the page.</p>
       default:
-        return <div>test</div>
+        return (
+          <div style={{padding: "20px 0", fontSize: "15pt", textAlign: "center"}}>
+            {drawResult(info)}
+            <div>
+              <button onClick={print} value="Print">Print</button>
+            </div>
+          </div>
+        );
     }
   }
 
