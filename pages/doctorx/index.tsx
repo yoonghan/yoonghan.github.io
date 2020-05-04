4abe97ba3d2ac25964a8f26e78e580792c64ff8f
Walcron <b_wars@yahoo.com>
2020-05-04 08:45:26 +0800

    Deployment fix

--- pages/doctorx/index.tsx
+++ pages/doctorx/index.tsx
@@ -1,167 +1,20 @@
 import * as React from "react";
 import HeaderOne from "../../components/HeaderOne";
 import Footer from "../../components/Footer";
-import QRCode from "qrcode";
-import { compose } from 'redux';
-import withMessenger, { IWithMessenger } from "../../hoc/withMessenger";
+import Doctorx from "../../components/Doctorx";
 
-enum EnumDisplayState {
-  QRCODE,
-  PRINTABLE,
-  CLOSED
-};
-
-const printSystem = (setDisplayState:any) => (msg:string) => {
-  console.log(msg, "connection status");
-  if(msg === "Disconnected") {
-    setDisplayState(EnumDisplayState.CLOSED);
-  }
+interface StatelessPage<P = {}> extends React.SFC<P> {
+  getInitialProps?: () => Promise<P>
 }
 
-const printEvent = (setDisplayState:any, setInfo:any) => (msg:string) => {
-  const info = JSON.parse(msg);
-  setDisplayState(EnumDisplayState.PRINTABLE);
-  setInfo(info);
-}
-
-const drawResult = (info: object, survey: any) => {
-  const {imgLocation, phone, fax, address, consensus, declaration} = survey;
-  return (
-    <div className="alignment">
-      <table>
-        <tbody>
-          <tr>
-            <td rowSpan={3}>
-              <img src={imgLocation}/>
-            </td>
-            <td>
-              {address}
-            </td>
-          </tr>
-          <tr>
-            <td>
-              {phone}
-            </td>
-          </tr>
-          <tr>
-            <td>
-              {fax}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      {drawInfo(info)}
-      <ol>
-        <li>{consensus}</li>
-        <li>{declaration}</li>
-      </ol>
-      <style jsx>{`
-        table {
-          margin: auto;
-        }
-        img {
-          width: 100px;
-          height: auto;
-        }
-        ol,.alignment {
-          text-align: left;
-        }
-      `}</style>
-    </div>
-  )
-}
-
-const drawInfo = (info:object) => {
-  const infos = [];
-  for (const property in info) {
-    infos.push(<div>{property}: {info[property]}</div>);
-  }
-  return infos;
-}
-
-const print = () => {
-  window.print();
-}
-
-interface DoctorxProps {
-  messengerApi: IWithMessenger;
-  survey: any;
-}
-
-const Doctorx:any = (props: DoctorxProps) => {
-  const {messengerApi, survey} = props;
-  const canvasref = React.useRef(null);
-  const canvas2ref = React.useRef(null);
-  const [displayState, setDisplayState] = React.useState(EnumDisplayState.QRCODE);
-  const [info, setInfo] = React.useState({});
-  const toFillUrl = "https://www.walcron.com/doctorx/formfill";
-  const toFillGoogleUrl = "https://docs.google.com/forms/d/e/1FAIpQLSeFqSnCnMQG9T8481HHhEwidLzgwefnfh3vUVebHpH0BfNYyA/viewform";
-  const _printSystem = React.useMemo(
-    ()=>printSystem(setDisplayState)
-    ,[displayState]);;
-  const _printEvent = React.useMemo(
-    ()=>printEvent(setDisplayState, setInfo)
-    ,[displayState]);
-
-  React.useEffect(()=>{
-    messengerApi.connect("doctorx", _printSystem, _printEvent);
-    if(canvasref !== null && canvasref.current !== null) {
-      QRCode.toCanvas(canvasref.current, toFillUrl, function (error) {
-        console.error("Canvas generation failed." + error);
-      })
-    }
-    if(canvas2ref !== null && canvas2ref.current !== null) {
-      QRCode.toCanvas(canvas2ref.current, toFillGoogleUrl, function (error) {
-        console.error("Canvas generation failed." + error);
-      })
-    }
-  },[]);
-
-  const _displayBody = () => {
-    switch(displayState) {
-      case EnumDisplayState.QRCODE:
-        return (
-          <div style={{display:"flex", flexDirection: "row"}}>
-            <div>
-              <span>CUSTOM Form</span>
-              <div style={{textAlign:"center", padding: "40px"}}>
-                <canvas ref={canvasref} className="container"/>
-              </div>
-            </div>
-            <div>
-              <span>GOOGLE Form</span>
-              <div style={{textAlign:"center", padding: "40px"}}>
-                <canvas ref={canvas2ref} className="container"/>
-              </div>
-            </div>
-            <div>
-              <span>NOT SCANNABLE Form</span>
-              <div style={{textAlign:"center", padding: "40px"}}>
-                <img src="https://i.pinimg.com/474x/d6/b1/b6/d6b1b6a619fdbbc777196a277c0fa049.jpg" style={{width:"200px"}}/>
-              </div>
-            </div>
-          </div>)
-      case EnumDisplayState.CLOSED:
-        return <p>Closed due to non activity, to restore refresh the page.</p>
-      default:
-        return (
-          <div style={{padding: "20px 0", fontSize: "15pt", textAlign: "center"}}>
-            {drawResult(info, survey)}
-            <div className="printhidden">
-              <button onClick={print} value="Print">Print</button>
-            </div>
-          </div>
-        );
-    }
-  }
-
-
+const DoctorxPage:StatelessPage<any> = (props: any) => {
+  const {survey} = props;
   return (
     <React.Fragment>
       <header>
         <HeaderOne title={"Project Doctor x"} isLined={true}/>
       </header>
-      { _displayBody() }
+      {survey !== null && <Doctorx survey={props.survey} />}
       <div className="printhidden">To create one, click <a href="/doctorx/questionaires">!!here!!</a></div>
       <Footer/>
       <style jsx>{`
@@ -177,21 +30,17 @@ const Doctorx:any = (props: DoctorxProps) => {
   );
 }
 
-const mapMessengerApi = (result:any) => ({messengerApi: result});
-
-const _doctorx = compose(
-  withMessenger(mapMessengerApi, true)
-)(Doctorx);
-
-(_doctorx as any).getInitialProps = async() => {
+DoctorxPage.getInitialProps = async() => {
   const { AUTH_API_CALL } = process.env;
   if (AUTH_API_CALL) {
     try {
+      console.log(AUTH_API_CALL);
       const res = await fetch(AUTH_API_CALL + '/api/survey');
       const json = await res.json();
       return {survey: json};
     }
     catch (err) {
+      console.log(err);
       return {survey: null};
     }
 
@@ -199,4 +48,4 @@ const _doctorx = compose(
   return {survey: null};
 };
 
-export default _doctorx;
+export default DoctorxPage;
