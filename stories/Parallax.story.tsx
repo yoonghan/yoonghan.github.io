a04895b3566cc9374ca22050922af2c018a26c4d
Walcron <b_wars@yahoo.com>
2020-08-16 08:11:07 +0800

    Reorg codes

--- stories/Parallax.story.tsx
+++ stories/Parallax.story.tsx
@@ -1,11 +1,33 @@
 import * as React from 'react';
 import { BASIC_MENU } from './menu';
 import { storiesOf } from '@storybook/react';
-import WelcomeScreen from '../components/WelcomeScreen';
+import ParallaxBanner from "../components/Parallax/Banner";
 
 const Component = () => {
   return <WelcomeScreen/>
 }
 
-const stories = storiesOf(`${BASIC_MENU}/Test`, module);
+const WelcomeScreen:React.FC<any> = () => {
+  const scrollContainerRef = React.useRef<HTMLDivElement>(null);
+
+  return (
+    <div className="outer-container">
+      <div className="container" ref={scrollContainerRef}>
+        <ParallaxBanner scrollContainer={scrollContainerRef}/>
+      </div>
+      <style jsx>{`
+        .outer-container {
+          position: relative;
+        }
+        .container {
+          width: 100vw;
+          height: 66vw;
+          overflow-y: scroll;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+const stories = storiesOf(`${BASIC_MENU}/Parallax`, module);
 stories.add('WelcomeScreen', Component);
