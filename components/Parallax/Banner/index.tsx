ddecc91b1599314ee0f7d6518bdc8d1545dbb712
Walcron <b_wars@yahoo.com>
2020-08-23 18:46:36 +0800

    Update banner.

--- components/Parallax/Banner/index.tsx
+++ components/Parallax/Banner/index.tsx
@@ -9,10 +9,11 @@ interface IBanner {
 }
 
 const Banner:React.FC<IBanner> = ({scrollContainer}) => {
-  const sectionHeight='330vh', sectionOneHeight='100vh', sectionTwoHeight='230vh';
+  const sectionHeight='300vh', sectionOneHeight='100vh', sectionTwoHeight='200vh';
   const backgroundRef = useRef<HTMLDivElement>(null);
   const leftForegroundRef = useRef<HTMLDivElement>(null);
   const rightForegroundRef = useRef<HTMLDivElement>(null);
+  const centerForegroundRef = useRef<HTMLDivElement>(null);
   const centerpieceTitleRef = useRef<HTMLDivElement>(null);
   const centerpieceDescRef = useRef<HTMLDivElement>(null);
   const parallaxDisplayContainerRef = useRef<HTMLDivElement>(null);
@@ -24,17 +25,49 @@ const Banner:React.FC<IBanner> = ({scrollContainer}) => {
     parallaxClientHeight= 0;
   const allowedDescToKickstart = 0.4;
   const allowedBackgroundScaledDistance = 5;
+  const firstPartCompletionDistance = 360;
 
   const parallaxScrollForeground = (e:any) => {
-    const pos = e.target.scrollTop;
+    const _pos = e.target.scrollTop;
+    const pos = _pos - firstPartCompletionDistance;
     const targetHeight = e.target.offsetHeight;
     const opacityPercentage = (pos + (targetHeight / 2)) / parallaxDisplayContainerHeightSize;
     const adjustedPos = pos > parallaxDisplayContainerWidthSize ? parallaxDisplayContainerWidthSize:pos;
     var scaledPos = adjustedPos / 200;
     var translatePos = adjustedPos * 2;
 
+    if(_pos < firstPartCompletionDistance) {
+      if(centerForegroundRef.current !== null) {
+        if(centerForegroundRef.current.style.display === 'none') {
+          centerForegroundRef.current.style.display = ``;
+          if(leftForegroundRef.current !== null && rightForegroundRef.current !== null) {
+            leftForegroundRef.current.children[0].style.display = 'none';
+            rightForegroundRef.current.children[0].style.display = 'none';
+            leftForegroundRef.current.style.transform = 'scale(1.0)';
+            rightForegroundRef.current.style.transform = 'scale(1.0)';
+          }
+        }
+
+        centerForegroundRef.current.style.transform = `rotateY(${_pos}deg)`;
+        centerForegroundRef.current.children[1].style.transform = `rotateY(${_pos * 2}deg)`;
+      }
+
+      return;
+    }
+    else {
+      if(centerForegroundRef.current !== null) {
+        if(centerForegroundRef.current.style.display !== 'none') {
+          centerForegroundRef.current.style.display = 'none';
+          if(leftForegroundRef.current !== null && rightForegroundRef.current !== null) {
+            leftForegroundRef.current.children[0].style.display = 'flex';
+            rightForegroundRef.current.children[0].style.display = 'flex';
+          }
+        }
+      }
+    }
+
     if(dividerRef !== null && pos < parallaxClientHeight) {
-      dividerRef.current.style.width=`${(1-(pos / parallaxClientHeight)) * 100}%`;
+      dividerRef.current.style.width=`${(0.9-(pos / parallaxClientHeight)) * 100}%`;
     }
 
     setScrollLen(Math.floor(pos));
@@ -52,6 +85,7 @@ const Banner:React.FC<IBanner> = ({scrollContainer}) => {
 
     if(centerpieceTitleRef.current !== null) {
       centerpieceTitleRef.current.style.opacity = `${1 - opacityPercentage}`;
+      centerpieceTitleRef.current.style.transform = `scale(${20 * opacityPercentage})`;
     }
     if(centerpieceDescRef.current !== null) {
       centerpieceDescRef.current.style.opacity = `${opacityPercentage - allowedDescToKickstart}`;
@@ -112,10 +146,20 @@ const Banner:React.FC<IBanner> = ({scrollContainer}) => {
           </p>
         </div>
         <div className="foreground-left" ref={leftForegroundRef}>
-          <div className="foreground-text left">Wal</div>
+          <div className="foreground-center left">
+            <div>Walcron</div>
+            <div className="before"/>
+          </div>
         </div>
         <div className="foreground-right" ref={rightForegroundRef}>
-          <div className="foreground-text">cron</div>
+          <div className="foreground-center right">
+            <div>Walcron</div>
+            <div className="before"/>
+          </div>
+        </div>
+        <div className="foreground-center" ref={centerForegroundRef}>
+          <div>Walcron</div>
+          <div className="before"/>
         </div>
       </div>
       <div className="divider" ref={dividerRef}></div>
@@ -150,6 +194,8 @@ const Banner:React.FC<IBanner> = ({scrollContainer}) => {
           background-size: cover;
           background-position: left bottom;
           background-color: black;
+          overflow: hidden;
+          border-right: 1px solid;
         }
         .foreground-right {
           will-change: transform;
@@ -161,6 +207,45 @@ const Banner:React.FC<IBanner> = ({scrollContainer}) => {
           background-size: cover;
           background-position: right bottom;
           background-color: black;
+          overflow: hidden;
+          border-left: 1px solid;
+        }
+        .foreground-center {
+          will-change: transform, display;
+          position: absolute;
+          left: calc(50% - 12rem);
+          top: calc(50% - 12rem);
+          width: 24rem;
+          height: 24rem;
+          border: 1rem solid rgba(51,153,67);
+          border-radius: 24rem;
+          transform-style: preserve-3d;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          color: ${HEADER_TITLE.FOREGROUND};
+          font-size: 5rem;
+        }
+        .foreground-center > .before {
+          content: '';
+          will-change: transform,display;
+          border: 5px solid rgba(51,153,67, 0.8);
+          position: absolute;
+          width: 18rem;
+          height: 18rem;
+          border-radius: 18rem;
+          transform-style: preserve-3d;
+        }
+        .foreground-center.left {
+          display: none;
+          left: unset;
+          right: 0;
+          transform: translateX(12rem);
+        }
+        .foreground-center.right {
+          display: none;
+          left: 0;
+          transform: translateX(-12rem);
         }
         .foreground-text {
           top: 50%;
@@ -191,7 +276,7 @@ const Banner:React.FC<IBanner> = ({scrollContainer}) => {
           position: absolute;
           top: 50%;
           left: 50%;
-          transform: translateX(-50%);
+          transform: translate(-50%, -50%);
           color: ${HEADER_TITLE.BACKGROUND};
         }
         .centerpiece.inverse {
