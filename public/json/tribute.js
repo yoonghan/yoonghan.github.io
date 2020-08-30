9ea3ab94f3d9605d1a5735d2860708b169a95385
Walcron <b_wars@yahoo.com>
2020-08-30 10:10:33 +0800

    Update UX

---
+++ public/json/tribute.js
@@ -0,0 +1,54 @@
+const tribute = [
+  {
+    "1": "Pusher",
+    "2": "Subscribe/Publish",
+    "3": "https://www.pusher.com"
+  },
+  {
+    "1": "Zeit / Now.sh",
+    "2": "Enabling website hosting",
+    "3": "https://www.zeit.co"
+  },
+  {
+    "1": "Author Ruslan Khomiak",
+    "2": "Graphics for social connect",
+    "3": "https://codepen.io/ruslan_khomiak/pen/QGmwMP"
+  },
+  {
+    "1": "Rebin from Iconfinder",
+    "2": "Graphic icon for social connect",
+    "3": "https://www.iconfinder.com/rebininfotech"
+  },
+  {
+    "1": "Font Awesome",
+    "2": "Awesome font icons",
+    "3": "https://www.fontawesome.com"
+  },
+  {
+    "1": "Google Fonts - Roboto",
+    "2": "The amazing fonts curves and styles",
+    "3": "https://fonts.google.com"
+  },
+  {
+    "1": "React Spring",
+    "2": "Might be the best animation javascript out there",
+    "3": "https://github.com/react-spring/react-spring"
+  },
+  {
+    "1": "React Bell Chat",
+    "2": "For use in chat program",
+    "3": "https://github.com/react-bell-chat"
+  },
+  {
+    "1": "React Dropzone",
+    "2": "For use in chat program",
+    "3": "https://github.com/react-dropzone"
+  },
+  {
+    "1": "React Autosuggest",
+    "2": "One of the best dropdown, though it's deprecating",
+    "3": "https://github.com/moroshko/react-autosuggest"
+  },
+];
+
+export default tribute;
