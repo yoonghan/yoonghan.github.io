fb4e8cb3244619586f11ecacfaed5b033ce50a3d
Walcron <b_wars@yahoo.com>
2020-09-24 11:33:27 +0800

    Move typescript d.ts check

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,32 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+declare module "react-coverflow" {
+  export interface CoverflowProps {
+    width: string;
+    height?: string;
+    displayQuantityOfSide: number;
+    navigation: boolean;
+    enableScroll: boolean;
+    clickable: boolean;
+    children: ReactNode;
+    active?: number;
+  }
+  class Coverflow extends React.Component<CoverflowProps, {}> {
+  }
+  export default Coverflow;
+}
+
+declare module "react-alert-template-basic" {
+  class AlertTemplate extends React.Component<any, any> {
+  }
+
+  export default AlertTemplate;
+}
+
+declare module "next-offline/runtime" {
+  class register {
+  }
+
+  class unregister {
+  }
+}
