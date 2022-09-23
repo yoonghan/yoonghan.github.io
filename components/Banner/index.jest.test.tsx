e17b663d98fd4bf6eb96f7989cfebfcee8eb7fe7
han <walcoorperation@gmail.com>
2022-09-23 23:22:43 +0800

    Add cookie.

--- components/Banner/index.jest.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import Banner from "."
-
-describe("Banner", () => {
-  it("renders a heading", () => {
-    render(<Banner />)
-    expect(screen.getByText("I am a Banner")).toBeInTheDocument()
-  })
-})
