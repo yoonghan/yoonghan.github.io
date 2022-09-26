d1338bb2f0a8e031225f729578680104b5dac131
han <walcoorperation@gmail.com>
2022-09-26 23:16:29 +0800

    Final copy

--- .github/scripts/snapshot-check.sh
+++ .github/scripts/snapshot-check.sh
@@ -2,6 +2,8 @@ if ls ./backstop_data/bitmaps_test/*/failed* 1> /dev/null 2>&1; then
     echo "Contains failed snapshot."
     echo "Download new snapshots in bitmaps_test and replace in reference if you are confident on the change."
     echo "Push new changes after the copy."
+    echo "---"
+    echo "For change comparison, locate it in ./html_report/index.html"
     exit 1
 fi
 exit 0
