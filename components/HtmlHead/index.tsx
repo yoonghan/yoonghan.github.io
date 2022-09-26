841c1feb95dcf7eabec0833ca8611ac28def9df3
han <walcoorperation@gmail.com>
2022-09-26 15:11:39 +0800

    Try error commit.

--- components/HtmlHead/index.tsx
+++ components/HtmlHead/index.tsx
@@ -11,6 +11,9 @@ const HtmlHead = ({
 }) => {
   return (
     <>
+      <head>
+        <title>Title</title>
+      </head>
       <Head>
         <meta charSet="utf-8" key="charset" />
         <meta
