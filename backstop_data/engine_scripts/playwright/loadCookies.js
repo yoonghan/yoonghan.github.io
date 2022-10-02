dc249c8cc542cff27f08c3f7cf9ae497b3b26be8
han <walcoorperation@gmail.com>
2022-10-02 11:37:39 +0800

    Update backstop with email inserted.

--- backstop_data/engine_scripts/playwright/loadCookies.js
+++ backstop_data/engine_scripts/playwright/loadCookies.js
@@ -3,11 +3,15 @@ const fs = require("fs")
 module.exports = async (browserContext, scenario) => {
   let cookies = []
   const cookiePath = scenario.cookiePath
+  const cookieDomain = scenario.cookieDomain
 
   // Read Cookies from File, if exists
-  if (fs.existsSync(cookiePath)) {
+  if (cookiePath && fs.existsSync(cookiePath)) {
     cookies = JSON.parse(fs.readFileSync(cookiePath))
   }
+
+  // Replace with cookie with domain
+  cookies = cookies.map((cookie) => ({ ...cookie, domain: cookieDomain }))
 
   // Add cookies to browser
   browserContext.addCookies(cookies)
