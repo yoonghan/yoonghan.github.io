18cbf9e88fa2db56002fdd9a4ecdf7057405e606
han <walcoorperation@gmail.com>
2022-10-04 00:40:51 +0800

    Update google tag.

--- components/HtmlHead/index.tsx
+++ components/HtmlHead/index.tsx
@@ -62,14 +62,13 @@ const HtmlHead = ({
 
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-9V9VC8N5XT"
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics" strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-      
         gtag('config', 'G-9V9VC8N5XT');
       `}
       </Script>
