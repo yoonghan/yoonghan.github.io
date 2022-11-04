1e06e3b055bd4a623c32b9aa5a2c6ff932c6d4ca
han <walcoorperation@gmail.com>
2022-11-05 01:04:17 +0800

    Removed links, not allowed

--- pages/homepage/index.tsx
+++ pages/homepage/index.tsx
@@ -13,11 +13,48 @@ import Button from "@/components/Button"
 import SocialFab from "@/components/SocialFab"
 import Navigator from "@/components/Navigator"
 import Cookie from "@/components/Cookie"
-import { links } from "./links"
 
 interface Props {
   termsRead: boolean
 }
+
+const links = [
+  {
+    id: "1",
+    desc: "Test Driven Development",
+    link: "1",
+  },
+  {
+    id: "2",
+    desc: "Github Pull Request",
+    link: "2",
+  },
+  {
+    id: "3",
+    desc: "Github Workflow",
+    link: "3",
+  },
+  {
+    id: "4",
+    desc: "Testing Deployment",
+    link: "4",
+  },
+  {
+    id: "5",
+    desc: "UI/UX Validation",
+    link: "5",
+  },
+  {
+    id: "6",
+    desc: "Going Live",
+    link: "6",
+  },
+  {
+    id: "7",
+    desc: "Contact Us",
+    link: "7",
+  },
+]
 
 function Home({ termsRead }: Props) {
   const scrollContainerRef = useRef<HTMLDivElement>(null)
