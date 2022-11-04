71ecf5cd39724f9ec28f02187f87850fd1410453
han <walcoorperation@gmail.com>
2022-11-05 06:51:42 +0800

    Add vercel deployment status check

---
+++ .github/scripts/deployment-check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+counter=1
+
+while [ $counter -le 6 ]
+do
+    status_code=$(curl --write-out %{http_code} --silent --output /dev/null $1)
+    if [[ "$status_code" -ne 200 ]]
+    then
+        sleep 5
+        counter=$((counter + 1))
+    else
+        exit 0
+    fi
+done
+
+echo "Site is still down $1"
+exit 1
--- .github/scripts/snapshot-check.sh
+++ .github/scripts/snapshot-check.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if ls ./backstop_data/bitmaps_test/*/failed* 1> /dev/null 2>&1; then
     echo "Contains failed snapshot."
     echo "Download new snapshots in bitmaps_test and replace in reference if you are confident on the change."
