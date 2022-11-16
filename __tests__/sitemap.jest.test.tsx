92d88851a64aa37c07760ad05c43cc8c63e8250d
han <walcoorperation@gmail.com>
2022-11-16 22:50:51 +0800

    Generate sitemap

---
+++ __tests__/sitemap.jest.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from "@testing-library/react"
+import SiteMap from "@/pages/sitemap"
+
+jest.mock("next/router", () => require("next-router-mock"))
+
+describe("About", () => {
+  const renderComponent = async () => {
+    render(<SiteMap />)
+    expect(await screen.findByText("walcron@tm$")).toBeInTheDocument()
+  }
+
+  it("should render the page with right links", async () => {
+    await renderComponent()
+    expect(screen.getByText("HOME")).toHaveAttribute("href", "/")
+    expect(screen.getByText("ABOUT US")).toHaveAttribute("href", "/about")
+    expect(screen.getByText("HISTORY")).toHaveAttribute("href", "/history")
+    expect(screen.getByText("PROJECTS")).toHaveAttribute("href", "/listing")
+  })
+})
