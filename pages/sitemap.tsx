f502ecf6a05465eb842cea49c9befbce1f9b6c39
Han <walcoorperation@gmail.com>
2022-11-16 23:16:09 +0800

    Develop (#54)
    * Generate sitemap
    
    * Update backstop check.
    
    * Update backstop to include listing.
    
    * Update with new lines.
    
    * Add sitemap module
    
    * Updae backstop

---
+++ pages/sitemap.tsx
@@ -0,0 +1,45 @@
+import CommandBar from "@/components/CommandBar"
+import Footer from "@/components/Footer"
+import HtmlHead from "@/components/HtmlHead"
+import { memo } from "react"
+import ScrollToTop from "@/components/ScrollToTop"
+import Link from "next/link"
+import HeaderOne from "@/components/HeaderOne"
+import styles from "@/pageComponents/Sitemap/Sitemap.module.css"
+
+const SiteMap = ({}) => {
+  return (
+    <>
+      <HtmlHead title={"Site Map"} description={"Website links and site."} />
+      <div>
+        <CommandBar />
+      </div>
+      <div className={`${styles.container} page-aligned-container`}>
+        <HeaderOne title={"Sitemap"} isLined={false} />
+        <ul className={"u-list"}>
+          <li>
+            <Link href="/">HOME</Link>
+          </li>
+          <li>
+            <span>SECTION</span>
+            <ul className={"u-list"}>
+              <li>
+                <Link href="/about">ABOUT US</Link>
+              </li>
+              <li>
+                <Link href="/history">HISTORY</Link>
+              </li>
+              <li>
+                <Link href="/listing">PROJECTS</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <Footer isRelative={true} />
+      <ScrollToTop />
+    </>
+  )
+}
+
+export default memo(SiteMap)
