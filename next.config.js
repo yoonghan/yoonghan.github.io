76cad36a158ccf7c3f8604be8507f3470cd4ffb5
Han <walcoorperation@gmail.com>
2022-11-19 00:05:59 +0800

    Reduce webpack code! (#57)

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 })
 
 module.exports = withBundleAnalyzer({
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.module.noParse = [require.resolve("typescript/lib/typescript.js")]
-    return config
-  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
