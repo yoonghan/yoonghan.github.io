b44e6ae0d2bd49a8888a295042436b03eee5ff4f
han <walcoorperation@gmail.com>
2022-11-20 20:13:40 +0800

    Add header and menu check for all sites.

--- __tests__/history.jest.test.tsx
+++ __tests__/history.jest.test.tsx
@@ -1,24 +1,35 @@
-import { render, screen, fireEvent } from "@testing-library/react"
+import { render, screen, fireEvent, within } from "@testing-library/react"
 import History from "@/pages/history"
+import { assertFooter } from "./utils/_footer"
+import { assertMenu } from "./utils/_menu"
 
 jest.mock("next/router", () => require("next-router-mock"))
 
 describe("History", () => {
-  const renderComponent = async () => {
+  const renderComponent = () => {
     render(<History />)
-    expect(await screen.findByText("walcron@tm$")).toBeInTheDocument()
   }
 
-  it("should render the page with the important components", async () => {
-    await renderComponent()
+  it("should have a menu", async () => {
+    renderComponent()
+    await assertMenu()
+  })
+
+  it("should render the page with the important components", () => {
+    renderComponent()
     expect(screen.getByText("Site's history"))
     expect(screen.getByText("Motivational books"))
   })
 
   it("should be able to scroll up", async () => {
-    await renderComponent()
+    renderComponent()
     expect(screen.queryByText("Up")).not.toBeInTheDocument()
     fireEvent.scroll(window, { target: { pageYOffset: 321 } })
     expect(screen.getByText("Up")).toBeInTheDocument()
+  })
+
+  it("should render the page with footer", async () => {
+    renderComponent()
+    assertFooter()
   })
 })
