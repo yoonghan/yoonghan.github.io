4c142891c3f6f390c3b8894fccbec57eda0f1977
han <walcoorperation@gmail.com>
2022-11-20 20:13:40 +0800

    Add header and menu check for all sites.

---
+++ __tests__/lessons.jest.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen, within } from "@testing-library/react"
+import Lessons from "@/pages/projects/lessons"
+import { assertFooter } from "./utils/_footer"
+import { assertMenu } from "./utils/_menu"
+
+jest.mock("next/router", () => require("next-router-mock"))
+
+describe("Lessons", () => {
+  const renderComponent = () => {
+    render(<Lessons />)
+  }
+
+  it("should have a menu", async () => {
+    renderComponent()
+    await assertMenu()
+  })
+
+  it("should render the page with footer", async () => {
+    await renderComponent()
+    assertFooter()
+  })
+})
