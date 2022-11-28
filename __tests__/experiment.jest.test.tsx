f4c1fc52f8841df9a348035e0b698f62fb439609
Han <walcoorperation@gmail.com>
2022-11-28 21:42:12 +0800

    Minor amp page fixes. (#70)

---
+++ __tests__/experiment.jest.test.tsx
@@ -0,0 +1,17 @@
+import Experiment, { config } from "@/pages/experiments"
+import { render, screen, waitFor, within } from "@testing-library/react"
+
+jest.mock("next/router", () => require("next-router-mock"))
+
+describe("Experiment", () => {
+  it("should render page correctly to inform we are in development phase", () => {
+    render(<Experiment />)
+    expect(
+      screen.getByText("Currently we are still in development phase for AMP.")
+    ).toBeInTheDocument()
+  })
+
+  it("should expose config as true", () => {
+    expect(config).toStrictEqual({ amp: true })
+  })
+})
