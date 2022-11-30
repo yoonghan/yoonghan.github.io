17d802b8d36e77aeba1c443f44a281a809cc5f9f
Han <walcoorperation@gmail.com>
2022-11-30 23:24:24 +0800

    Add playwright (#76)
    * Fix canonical having value
    
    * Add playwright to check sitemap.xml exist.
    
    * Allow to check robots.txt
    
    * Update playwright command.
    
    * Upate webserver from running existing webserver.
    
    * Update backstop

--- __tests__/projects/lessons.jest.test.tsx
+++ __tests__/projects/lessons.jest.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, within } from "@testing-library/react"
 import Lessons from "@/pages/projects/lessons"
-import { assertFooter } from "./utils/_footer"
-import { assertMenu } from "./utils/_menu"
+import { assertFooter } from "../utils/_footer"
+import { assertMenu } from "../utils/_menu"
 
 jest.mock("next/router", () => require("next-router-mock"))
 
