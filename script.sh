09b0d38cd35706c175151488abc92c44409bdc0e
Han <walcoorperation@gmail.com>
2022-12-03 08:52:44 +0800

    Add simple script. (#85)

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Useful scripts for check in an checkout.
+git config --global alias.dd "branch -D develop"
+git config --global alias.db "branch develop"
+git config --global alias.dc "checkout develop"
