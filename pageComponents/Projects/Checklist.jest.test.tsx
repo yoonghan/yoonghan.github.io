036f6dfb3365757a9c7aead8d288f978a636c448
han <walcoorperation@gmail.com>
2022-12-05 21:16:07 +0800

    Fix test case due to checklist update

--- pageComponents/Projects/Checklist.jest.test.tsx
+++ pageComponents/Projects/Checklist.jest.test.tsx
@@ -15,25 +15,32 @@ describe("Checklist", () => {
     it("should render correctly", () => {
       render(<TroubleshootPwaCheckList />)
       expect(screen.getByText("PWA Registered")).toBeInTheDocument()
-      expect(screen.getByText("Trusted Site App")).toBeInTheDocument()
-      expect(screen.getByText("Detected New Update?")).toBeInTheDocument()
-      expect(screen.getByText("Update Installed?")).toBeInTheDocument()
+      expect(
+        screen.getByText("Trusted Site App (N/R, query string issue)")
+      ).toBeInTheDocument()
+      expect(screen.getByText("Detected New Update (N/T)")).toBeInTheDocument()
+      expect(screen.getByText("Update Installed (N/T)")).toBeInTheDocument()
+      expect(screen.getByText("PWA run offline")).toBeInTheDocument()
     })
 
     it("should be able to set all service to true, except for twa", async () => {
       setServiceNavigator()
       render(<TroubleshootPwaCheckList />)
-      expect(await screen.findAllByText("true")).toHaveLength(3)
+      expect(await screen.findAllByText("true")).toHaveLength(4)
       assertValue(screen.getByText("PWA Registered"), true)
-      assertValue(screen.getByText("Detected New Update?"), true)
-      assertValue(screen.getByText("Update Installed?"), true)
+      assertValue(screen.getByText("Detected New Update (N/T)"), true)
+      assertValue(screen.getByText("Update Installed (N/T)"), true)
+      assertValue(screen.getByText("PWA run offline"), true)
     })
 
     it("should be able to set twa to true", async () => {
       window.location.href = "?utm_source=launcher"
       render(<TroubleshootPwaCheckList />)
       expect(await screen.findAllByText("true")).toHaveLength(1)
-      assertValue(screen.getByText("Trusted Site App"), true)
+      assertValue(
+        screen.getByText("Trusted Site App (N/R, query string issue)"),
+        true
+      )
     })
   })
 })
