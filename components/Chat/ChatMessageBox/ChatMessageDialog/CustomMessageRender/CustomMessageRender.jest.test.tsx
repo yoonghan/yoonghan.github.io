6dc47bd5cabf139c6dae03c4791e9e7bf4eb5245
Han <walcoorperation@gmail.com>
2022-12-28 17:45:46 +0800

    Add pop up dialog to view message. (#101)
    * Add confirmation before download.
    
    * Make fixes due to submit button declared twice.
    
    * Fix bug on not allowing same file being uploaded.

--- components/Chat/ChatMessageBox/ChatMessageDialog/CustomMessageRender/CustomMessageRender.jest.test.tsx
+++ components/Chat/ChatMessageBox/ChatMessageDialog/CustomMessageRender/CustomMessageRender.jest.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import CustomMessageRender from "."
+import "../../../../../__mocks__/windowMock"
 
 describe("CustomMessageRender", () => {
   it("should render the component message correctly", () => {
@@ -14,13 +16,63 @@ describe("CustomMessageRender", () => {
     expect(screen.getByText("I am a complex message")).toBeInTheDocument()
   })
 
-  it("should render message as a html link", () => {
+  it("should render message as a button link and when user click no it does nothing", async () => {
     const link = "http://www.firebase.com/storage/testfile"
     render(<CustomMessageRender message={{ message: `F|${link}` }} />)
-    const messageField = screen.getByRole("link", {
-      name: "[File Received]",
-    })
-    expect(messageField).toHaveAttribute("href", link)
-    expect(messageField).toHaveAttribute("target", "_blank")
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: "Open file ?",
+      })
+    )
+    expect(
+      await screen.findByText("Download Unverified File")
+    ).toBeInTheDocument()
+
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: "No",
+      })
+    )
+    expect(
+      screen.queryByText("Download Unverified File")
+    ).not.toBeInTheDocument()
+
+    expect(
+      screen.getByRole("button", {
+        name: "Open file ?",
+      })
+    ).toBeInTheDocument()
+  })
+
+  it("should render message as a button link and when user click yes it redirects to new location", async () => {
+    const link = "http://www.firebase.com/storage/testfile"
+    render(<CustomMessageRender message={{ message: `F|${link}` }} />)
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: "Open file ?",
+      })
+    )
+    expect(
+      await screen.findByText("Download Unverified File")
+    ).toBeInTheDocument()
+
+    await userEvent.click(
+      screen.getByRole("button", {
+        name: "Yes",
+      })
+    )
+    expect(
+      screen.queryByText("Download Unverified File")
+    ).not.toBeInTheDocument()
+
+    expect(
+      screen.queryByRole("button", {
+        name: "Open file ?",
+      })
+    ).not.toBeInTheDocument()
+
+    const downloadAnchor = screen.getByRole("link", { name: "[File Received]" })
+    expect(downloadAnchor).toHaveAttribute("href", link)
+    expect(downloadAnchor).toHaveAttribute("target", "_blank")
   })
 })
