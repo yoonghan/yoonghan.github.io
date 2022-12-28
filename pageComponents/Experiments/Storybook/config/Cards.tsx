4a4b6eea134d6bc230c182d754b7dc65a1913fa9
han <walcoorperation@gmail.com>
2022-12-29 04:23:28 +0800

    Fix card display.

---
+++ pageComponents/Experiments/Storybook/config/Cards.tsx
@@ -0,0 +1,38 @@
+import Card from "@/components/Card"
+
+const Cards = (
+  <div style={{ background: "#222" }}>
+    <Card
+      cards={[
+        {
+          id: "1",
+          title: "Sample 1",
+          description: "Lorem ipsum lore",
+          href: "https://www.walcron.com",
+        },
+        {
+          id: "2",
+          title: "Sample 2",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare ipsum quis commodo faucibus. Nam gravida blandit turpis ut pretium. In dui dui, feugiat dictum vestibulum sed, vulputate ac mi. Nunc sit amet tempor dolor, sed viverra dui. Nunc aliquam sodales ante at dignissim. Cras cursus ipsum ut tincidunt faucibus. Phasellus tincidunt, turpis ullamcorper laoreet pellentesque, lorem tortor aliquet quam, vitae tristique quam magna non sapien. Cras scelerisque diam scelerisque, feugiat neque ut, viverra dui. Curabitur non ornare velit. In ac ipsum cursus, mattis erat vitae, ornare orci. Integer ac tortor libero.",
+          href: "https://www.walcron.com",
+        },
+        {
+          id: "3",
+          title: "Sample Long Title Of Many",
+          description: "Short message comprendo.",
+          href: "https://www.walcron.com",
+        },
+        {
+          id: "4",
+          title: "Sample 4",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare ipsum quis commodo faucibus. Nam gravida blandit turpis ut pretium. In dui dui, feugiat dictum vestibulum sed, vulputate ac mi. Nunc sit amet tempor dolor, sed viverra dui. Nunc aliquam sodales ante at dignissim. Cras cursus ipsum ut tincidunt faucibus. Phasellus tincidunt, turpis ullamcorper laoreet pellentesque, lorem tortor aliquet quam, vitae tristique quam magna non sapien. Cras scelerisque diam scelerisque, feugiat neque ut, viverra dui. Curabitur non ornare velit. In ac ipsum cursus, mattis erat vitae, ornare orci. Integer ac tortor libero.",
+          href: "https://www.walcron.com",
+        },
+      ]}
+    />
+  </div>
+)
+
+export default Cards
