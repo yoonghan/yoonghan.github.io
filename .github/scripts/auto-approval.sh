81b2e4a3e69fcdcdb7d81ce15dae58ea267b96b0
han <walcoorperation@gmail.com>
2022-12-30 04:34:11 +0800

    Add auto approval

---
+++ .github/scripts/auto-approval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APPROVE_RUN_NUMBER=`cat ../../backstop.approve`
+
+echo github [$GITHUB_RUN_NUMBER]
+echo approve [$APPROVE_RUN_NUMBER]
+echo
+if [ "$APPROVE_RUN_NUMBER" = "$GITHUB_RUN_NUMBER" ]; then
+    echo 'auto approving backstop'
+    npm run backstop:approve
+    rm ./backstop_data/bitmaps_test/*/failed*
+    git add ./backstop_data/*
+    git commit -m "Auto Approval"
+    git push
+fi
+exit 0
