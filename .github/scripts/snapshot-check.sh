3014b25151e7948be3c083c5ebd81c2f73fd4632
han <walcoorperation@gmail.com>
2022-12-30 13:18:18 +0800

    Update readme.

--- .github/scripts/snapshot-check.sh
+++ .github/scripts/snapshot-check.sh
@@ -5,9 +5,7 @@ if ls ./backstop_data/bitmaps_test/*/failed* 1> /dev/null 2>&1; then
     echo "Download new snapshots in bitmaps_test and replace in reference if you are confident on the change."
     echo "Push new changes after the copy."
     echo " OR "
-    echo "For auto approval, do echo $GITHUB_RUN_NUMBER + 1 > backstop.approve file."
-    echo "---"
-    echo "Push new changes after updating snapshot of backstop.approve creation file."
+    echo "Run Action with 'Create Approved Snapshot'"
     echo "---"
     echo "For change comparison, locate it in ./html_report/index.html"
     exit 1
