4b84cac36172dcdce8e022cf7d4ab221aa4364ef
han <walcoorperation@gmail.com>
2023-01-14 16:02:54 +0800

    Add react confirmation box.

--- src/pageComponents/Experiments/Storybook/config/Dialogs.tsx
+++ src/pageComponents/Experiments/Storybook/config/Dialogs.tsx
@@ -2,8 +2,25 @@ import Dialog from "@/components/Dialog"
 import ConfirmationDialog from "@/components/Dialog/ConfirmationDialog"
 import { useState } from "react"
 
+import { confirmable, createConfirmation } from "react-confirm"
+
 const Dialogs = () => {
   const [showDialogNo, setShowDialogNo] = useState(0)
+  const DialogManual = (props: any) => {
+    return (
+      <Dialog onCancel={() => setShowDialogNo(0)}>
+        <div>I am dialog 3 and I too do not even have a background!</div>
+      </Dialog>
+    )
+  }
+
+  const confirm = createConfirmation(confirmable(DialogManual))
+
+  const showDialog3 = async () => {
+    await confirm({
+      confirmation: "Are you sure?",
+    })
+  }
 
   return (
     <>
@@ -11,6 +28,7 @@ const Dialogs = () => {
       <button onClick={() => setShowDialogNo(2)}>
         Show Confirmation Dialog
       </button>
+      <button onClick={showDialog3}>Show Dialog</button>
       {showDialogNo === 1 && (
         <Dialog onCancel={() => setShowDialogNo(0)}>
           <div>HAHAHAHA, I Don&apos;t even have a background!</div>
