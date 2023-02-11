b1f65334192e03a1f21dc1630c8f7cb4dc13dfd9
Han <walcoorperation@gmail.com>
2023-02-12 01:20:54 +0800

    Animate menu. (#127)
    * Add animation to menu to switch to command.
    
    * Remove menu from amp, not compatible for now.
    
    * Fix experiment test case.
    
    * Add commandbar menu view.
    
    * Add backstop local approved updates.
    
    * Bump auto approval

--- src/components/Menu/Menu.jest.test.tsx
+++ src/components/Menu/Menu.jest.test.tsx
@@ -7,7 +7,11 @@ import Menu from "."
 jest.mock("next/router", () => require("next-router-mock"))
 
 describe("Menu", () => {
-  it("should load only top level testcase", () => {
+  const waitForCommandBar = async () => {
+    expect(await screen.findByText("walcron@tm$")).toBeInTheDocument()
+  }
+
+  it("should load only top level testcase", async () => {
     render(<Menu />)
     expect(screen.getByRole("img", { name: "home" })).toBeInTheDocument()
     expect(screen.getByText("Projects")).toBeInTheDocument()
@@ -16,31 +20,34 @@ describe("Menu", () => {
     expect(screen.getAllByText("|")).toHaveLength(
       sortedMenuPagesWithFilteredHomeAndSubMenu.length - 1
     )
+    await waitForCommandBar()
   })
 
-  it("should redirect to the right link, randomly picking project", () => {
+  it("should redirect to the right link, randomly picking project", async () => {
     render(<Menu />)
     const projectLink = screen.getByText("Projects")
     expect(projectLink).toHaveAttribute("href", "/projects")
+    expect(await screen.findByText("walcron@tm$")).toBeInTheDocument()
+    await waitForCommandBar()
   })
 
-  it("should show experiments submenus and current selected path is non attributed", () => {
+  it("should show experiments submenus and current selected path is non attributed", async () => {
     singletonRouter.push("/experiments")
     render(<Menu />)
     expect(screen.getByText("Accelerated Mobile Pages")).toBeInTheDocument()
     const experimentPath = screen.getByText("Experiments")
     expect(experimentPath).not.toHaveAttribute("href", "/experiments")
+    await waitForCommandBar()
   })
 
   it("should change when toggle with right, left pointer", async () => {
     render(<Menu />)
     const leftArrow = "〈"
-    const rightArrow = "〉"
     expect(screen.getByText("Projects")).toBeInTheDocument()
     await userEvent.click(screen.getByText(leftArrow))
     expect(await screen.findByText("walcron@tm$")).toBeInTheDocument()
-    await userEvent.click(screen.getByText(rightArrow))
-    expect(screen.getByText("Projects")).toBeInTheDocument()
-    expect(screen.getByText(leftArrow)).toBeInTheDocument()
+    await userEvent.click(screen.getByText(leftArrow))
+    expect(await screen.findByText("Projects")).toBeInTheDocument()
+    await waitForCommandBar()
   })
 })
