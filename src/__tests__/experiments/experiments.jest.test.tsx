48741958a7b4356fec3a8121ae2175ab1cf8da81
Han <walcoorperation@gmail.com>
2023-02-12 01:20:54 +0800

    Animate menu. (#127)
    * Add animation to menu to switch to command.
    
    * Remove menu from amp, not compatible for now.
    
    * Fix experiment test case.
    
    * Add commandbar menu view.
    
    * Add backstop local approved updates.
    
    * Bump auto approval

--- src/__tests__/experiments/experiments.jest.test.tsx
+++ src/__tests__/experiments/experiments.jest.test.tsx
@@ -1,10 +1,11 @@
 import Experiment, { config } from "@/pages/experiments"
 import { render, screen } from "@testing-library/react"
+import { assertMenu } from "../utils/_menu"
 
 jest.mock("next/router", () => require("next-router-mock"))
 
 describe("Experiments", () => {
-  it("should render page correctly to inform we are in development phase", () => {
+  it("should render page correctly to inform we are in development phase", async () => {
     render(<Experiment />)
     expect(
       screen.getByText("Currently we are still in development phase for AMP.")
