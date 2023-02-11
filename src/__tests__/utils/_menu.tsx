759081f16102fee67df4ad22c1f5b0e49ccc125b
Han <walcoorperation@gmail.com>
2023-02-12 01:20:54 +0800

    Animate menu. (#127)
    * Add animation to menu to switch to command.
    
    * Remove menu from amp, not compatible for now.
    
    * Fix experiment test case.
    
    * Add commandbar menu view.
    
    * Add backstop local approved updates.
    
    * Bump auto approval

--- src/__tests__/utils/_menu.tsx
+++ src/__tests__/utils/_menu.tsx
@@ -2,4 +2,5 @@ import { screen } from "@testing-library/react"
 
 export const assertMenu = async () => {
   expect(screen.getByRole("img", { name: "home" })).toBeInTheDocument()
+  expect(await screen.findByText("walcron@tm$")).toBeInTheDocument()
 }
