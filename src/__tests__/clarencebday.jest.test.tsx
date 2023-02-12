90099059b507a1dcb0c3c807a8cb8110f8174f5f
han <walcoorperation@gmail.com>
2023-02-12 23:55:03 +0800

    Fix test case.

--- src/__tests__/clarencebday.jest.test.tsx
+++ src/__tests__/clarencebday.jest.test.tsx
@@ -4,7 +4,9 @@ import { render, screen, fireEvent } from "@testing-library/react"
 describe("Clarence Birthday page", () => {
   it("should render as clarence bday page", () => {
     render(<ClarenceBday />)
-    expect(screen.getByText("Welcome To Clarence Birthday")).toBeInTheDocument()
+    expect(
+      screen.getByText("Welcome To Clarence's Birthday")
+    ).toBeInTheDocument()
   })
 
   it("should expose config with runtime set to nodejs as edge will not work", () => {
