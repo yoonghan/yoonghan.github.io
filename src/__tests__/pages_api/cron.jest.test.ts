6077fddfc6d1b67557a8f0309e7b099095696d18
han <walcoorperation@gmail.com>
2023-02-27 11:02:01 +0800

    Log cron calling method to check vercel execution.

--- src/__tests__/pages_api/cron.jest.test.ts
+++ src/__tests__/pages_api/cron.jest.test.ts
@@ -1,77 +1,13 @@
 import "../../__mocks__/fetchMock"
-import { prismaMock } from "../../__mocks__/prismaMock"
 import { mockResponse, NextApiRequestMock } from "../../__mocks__/apiMock"
 import { config } from "@/pages/api/cron"
-import { execute } from "@/pages/api/cron/_cron"
 
 describe("cron", () => {
-  describe("with execute module", () => {
-    it("should return an OK when data can be tracked", async () => {
-      prismaMock.cronJob.create.mockResolvedValue({
-        id: 1,
-        jobName: "Test CronJob",
-        createdAt: new Date(),
-      })
-      const response = await execute()
-      expect(response.status).toBe(200)
-      expect(await response.json()).toStrictEqual({
-        message: "Posted a cron job",
-      })
-    })
-
-    const assertFailCases = async (
-      error: Error | string,
-      expectedErrorMessage: string
-    ) => {
-      prismaMock.cronJob.create.mockRejectedValue(error)
-      const response = await execute()
-      expect(response.status).toBe(500)
-      expect(await response.json()).toStrictEqual({
-        message: "Fail to write",
-        error: expectedErrorMessage,
-      })
-    }
-
-    it("should return Fail when data cannot be written into DB", async () => {
-      const errorMessage = "Oops this should not happen"
-      assertFailCases(new Error(errorMessage), errorMessage)
-    })
-
-    it("should return Fail when data cannot be written into DB with improper error object", async () => {
-      const errorMessage = "Oops this should not happen"
-      assertFailCases(errorMessage, errorMessage)
-    })
-
-    it("should display listing when passed with view = history query", async () => {
-      const historyResult = [
-        {
-          id: 1,
-          jobName: "Test CronJob 1",
-          createdAt: new Date(),
-        },
-        {
-          id: 2,
-          jobName: "Test CronJob 2",
-          createdAt: new Date(),
-        },
-      ]
-      prismaMock.cronJob.findMany.mockResolvedValue(historyResult)
-      const response = await execute("history")
-      expect(response.status).toBe(200)
-      expect(await response.json()).toStrictEqual(
-        historyResult.map((it) => ({
-          ...it,
-          createdAt: `${it.createdAt.toISOString()}`,
-        }))
-      )
-    })
-  })
-
   it("should use nodejs runtime", () => {
     expect(config).toStrictEqual({ runtime: "nodejs" })
   })
 
-  it("should call execute with correct header", async () => {
+  it("should call execute with correct query and method", async () => {
     const setHeaderFn = jest.fn()
     const setStatusFn = jest.fn()
     const mockExecute = jest.fn()
@@ -81,7 +17,7 @@ describe("cron", () => {
           status: 200,
         })
     )
-    jest.mock("@/pages/api/cron/_cron", () => {
+    jest.mock("@/pageComponents/api/cron/module", () => {
       return {
         execute: mockExecute,
       }
@@ -89,10 +25,11 @@ describe("cron", () => {
     const cronJob = require("@/pages/api/cron").default
     const nextRequest = new NextApiRequestMock({
       query: { view: "history" },
+      method: "POST",
     })
     const { nextResponse } = mockResponse(setHeaderFn, setStatusFn)
     await cronJob(nextRequest, nextResponse)
-    expect(mockExecute).toBeCalledWith("history")
+    expect(mockExecute).toBeCalledWith("history", "POST")
     expect(setHeaderFn).toBeCalledWith("Content-Type", "application/json")
     expect(setStatusFn).toBeCalledWith(200)
   })
