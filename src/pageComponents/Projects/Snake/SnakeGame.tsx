ca1ab1eb6a57647df48a0fa8559cff9ea98cd1d8
Han <walcoorperation@gmail.com>
2023-03-27 11:54:59 +0800

    Add Snake Game (#150)
    * Update footer styling.
    
    * Update to useRouter for nextJs 13.
    
    * Update next/router to AppRouterInstance
    
    * Bump nextJS 13
    
    * Update test coverage.
    
    * Fix ordering, making projects as priority.
    
    * Allow sub menu sorting.
    
    * Added rust and Snake Game - No Test Cases
    
    * Added rust and Snake Game - No Test Cases
    
    * Update gitignore to include pkg folder
    
    * Add pkg
    
    * Update page order.
    
    * Bump backstop approver.

---
+++ src/pageComponents/Projects/Snake/SnakeGame.tsx
@@ -0,0 +1,134 @@
+import { ChangeEvent, useCallback, useEffect, useState } from "react"
+import init, { InitOutput } from "snake-game"
+import { rnd } from "src/util/random"
+import Board from "./Board"
+import { GameContext } from "./Context"
+import styles from "./Snake.module.css"
+
+const WORLD_DIMENSION = 20
+const SNAKE_POS = rnd(WORLD_DIMENSION * WORLD_DIMENSION)
+const SNAKE_SIZE = 10
+const CELL_SIZE = 20
+const SPEED = 10
+
+const SnakeGame = () => {
+  const [isGameStarted, setGameStarted] = useState(false)
+  const [worldDimension, setWorldDimension] = useState(WORLD_DIMENSION)
+  const [snakeSpeed, setSnakeSpeed] = useState(SPEED)
+  const [snakePos, setSnakePos] = useState(SNAKE_POS)
+  const [snakeSize, setSnakeSize] = useState(SNAKE_SIZE)
+  const [cellSize, setCellSize] = useState(CELL_SIZE)
+  const [wasm, setWasm] = useState<InitOutput>()
+
+  useEffect(() => {
+    init().then((wasm) => {
+      setWasm(wasm)
+    })
+  }, [])
+
+  const updateInput = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const targetName = event.target.id
+      const value = parseInt(event.target.value, 10)
+      if (Number.isNaN(value) || value <= 0 || value > 100) {
+        return
+      }
+      switch (targetName) {
+        case "worldDimension": {
+          setWorldDimension(value)
+          break
+        }
+        case "snakeSize": {
+          if (value < worldDimension) {
+            setSnakeSize(value)
+            setSnakePos(rnd(worldDimension * worldDimension))
+          }
+          break
+        }
+        case "cellSize": {
+          setCellSize(value)
+          break
+        }
+        case "snakeSpeed": {
+          setSnakeSpeed(value)
+          break
+        }
+      }
+    },
+    [worldDimension]
+  )
+
+  return (
+    <GameContext.Provider value={{ isGameStarted, setGameStarted }}>
+      <div className={styles.container}>
+        <GameContext.Consumer>
+          {({ isGameStarted }) => (
+            <form onSubmit={() => false}>
+              <fieldset
+                disabled={isGameStarted}
+                className={styles.configuration}
+              >
+                <div>
+                  Snake Speed (sec/100):
+                  <input
+                    type="number"
+                    max="100"
+                    min="1"
+                    onChange={updateInput}
+                    value={snakeSpeed}
+                    id="snakeSpeed"
+                  />
+                </div>
+                <div>
+                  World Dimension:
+                  <input
+                    type="number"
+                    max="100"
+                    min="1"
+                    onChange={updateInput}
+                    value={worldDimension}
+                    id="worldDimension"
+                  />
+                </div>
+                <div>
+                  Snake Size:
+                  <input
+                    type="number"
+                    max="100"
+                    min="1"
+                    onChange={updateInput}
+                    value={snakeSize}
+                    id="snakeSize"
+                  />
+                </div>
+                <div>
+                  Cell Size:
+                  <input
+                    type="number"
+                    max="100"
+                    min="1"
+                    onChange={updateInput}
+                    value={cellSize}
+                    id="cellSize"
+                  />
+                </div>
+              </fieldset>
+            </form>
+          )}
+        </GameContext.Consumer>
+        {wasm && (
+          <Board
+            worldDimension={worldDimension}
+            snakePos={snakePos}
+            snakeSize={snakeSize}
+            snakeSpeed={snakeSpeed}
+            cellSize={cellSize}
+            wasm={wasm}
+          />
+        )}
+      </div>
+    </GameContext.Provider>
+  )
+}
+
+export default SnakeGame
