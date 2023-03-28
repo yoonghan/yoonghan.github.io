bce19b9cd770c6ed66c0a99d18f51adc7dff2a6c
han <walcoorperation@gmail.com>
2023-03-29 04:35:43 +0800

    Add workflow check compiled wasm

--- .github/scripts/wasm-check.sh
+++ .github/scripts/wasm-check.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+# Can't compare *.wasm file as binary compilation are different
+
 wasm_dir_compare="./$1/"
 wasm_dir=./pkg/
 
-for file in ./$wasm_dir_compare/*.wasm* ; do
+function cmpFile() {
+    fileName="${1##*/}"
+    echo "comparing $fileName"
+    cmp -b $wasm_dir_compare$fileName $wasm_dir$fileName
+    if [[ $? -ne 0 ]]; then
+        echo "Please recompile WASM with $fileName"
+        exit 1
+    fi
+}
+
+for file in ./$wasm_dir_compare/*.ts ; do
     [ -L "${file%/}" ] && continue
-    fileName="${file##*/}"
-    cmp "$wasm_dir_compare$fileName" "$wasm_dir$fileName"
+    cmpFile $file
+done
+
+for file in ./$wasm_dir_compare/*.js ; do
+    [ -L "${file%/}" ] && continue
+    cmpFile $file
 done
