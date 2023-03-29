6880acdfd7f5e49c9575fa632485967a5f506a1e
Han <walcoorperation@gmail.com>
2023-03-29 19:28:58 +0800

    Enchance Rust CI/CD (#153)
    * Remove wee_alloc from rush
    
    * Add Rust licensing.
    
    * Add rust compiler to git action.
    
    * Allow Rust Cargo.lock be checked in
    
    * Add wasm detection
    
    * Add workflow check compiled wasm

---
+++ .github/scripts/wasm-check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Can't compare *.wasm file as binary compilation are different
+
+wasm_dir_compare="./$1/"
+wasm_dir=./pkg/
+
+function cmpFile() {
+    fileName="${1##*/}"
+    echo "comparing $fileName"
+    cmp -b $wasm_dir_compare$fileName $wasm_dir$fileName
+    if [[ $? -ne 0 ]]; then
+        echo "Please recompile WASM with $fileName"
+        exit 1
+    fi
+}
+
+for file in ./$wasm_dir_compare/*.ts ; do
+    [ -L "${file%/}" ] && continue
+    cmpFile $file
+done
+
+for file in ./$wasm_dir_compare/*.js ; do
+    [ -L "${file%/}" ] && continue
+    cmpFile $file
+done
