35e7985a11b860e908b90eab81f483149a31fab9
han <walcoorperation@gmail.com>
2023-03-31 17:21:52 +0800

    Build and deploy as it is.

--- crate/tests/web.rs
+++ crate/tests/web.rs
@@ -8,3 +8,5 @@ fn lib_test() {
     //dummy, not sure how to do a test here. It's suppose to run a browser test!
     assert_eq!(1 + 1, 2);
 }
+
+//need reference to https://rustwasm.github.io/docs/book/game-of-life/rules.html and https://github.com/zademn/game-of-life-rust
