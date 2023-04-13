ce3f77995080df723924c3497d913883028e6eea
han <walcoorperation@gmail.com>
2023-04-14 05:58:52 +0800

    Update backstop approval and remove js check for WASM

--- .github/scripts/wasm-check.sh
+++ .github/scripts/wasm-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Can't compare *.wasm file as binary compilation are different
+# Can't compare js., order of generated function is different.
 
 cd crate
 
@@ -13,16 +14,13 @@ function cmpFile() {
     cmp -b $wasm_dir_compare$fileName $wasm_dir$fileName
     if [[ $? -ne 0 ]]; then
         echo "Please recompile WASM with $fileName"
+        echo "---content---"
+        cat $wasm_dir_compare$fileName
         exit 1
     fi
 }
 
 for file in ./$wasm_dir_compare/*.ts ; do
-    [ -L "${file%/}" ] && continue
-    cmpFile $file
-done
-
-for file in ./$wasm_dir_compare/*.js ; do
     [ -L "${file%/}" ] && continue
     cmpFile $file
 done
