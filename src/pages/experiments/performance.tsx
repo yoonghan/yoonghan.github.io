932680771144db0214d42d56da8f91c4423acc52
han <walcoorperation@gmail.com>
2023-04-20 06:03:24 +0800

    Large form update experimentation.

--- src/pages/experiments/performance.tsx
+++ src/pages/experiments/performance.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer"
 import ScrollToTop from "@/components/ScrollToTop"
 import styles from "@/pageComponents/Experiments/Performance/Performance.module.css"
 import Button from "@/components/Button"
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 const HeavyLoaderNoSSR = dynamic(
   () => import("@/pageComponents/Experiments/Performance/HeavyLoader"),
@@ -33,9 +33,77 @@ const SuspenseNoSSR = dynamic(
   }
 )
 
+const LargeFormNoSSR = dynamic(
+  () => import("@/pageComponents/Experiments/Performance/LargeForm"),
+  {
+    ssr: false,
+    loading: () => (
+      <div style={{ fontFamily: "Inconsolata", color: "green" }}>
+        Loading Large Form
+      </div>
+    ),
+  }
+)
+
+const LargeForm2NoSSR = dynamic(
+  () => import("@/pageComponents/Experiments/Performance/LargeForm2"),
+  {
+    ssr: false,
+    loading: () => (
+      <div style={{ fontFamily: "Inconsolata", color: "green" }}>
+        Loading Large Form 2
+      </div>
+    ),
+  }
+)
+
+const ReactTrackedNoSSR = dynamic(
+  () => import("@/pageComponents/Experiments/Performance/ReactTracked"),
+  {
+    ssr: false,
+    loading: () => (
+      <div style={{ fontFamily: "Inconsolata", color: "green" }}>
+        Loading React Tracked
+      </div>
+    ),
+  }
+)
+
 const Performance = () => {
-  const [showHeavyLoader, setShowHeavyLoader] = useState(false)
-  const [showSuspense, setShowSuspense] = useState(false)
+  const [shown, setShown] = useState<{ [string: string]: boolean }>({
+    heavyLoader: false,
+    suspend: false,
+    largeForm: false,
+    largeForm2: false,
+    reactTracked: false,
+  })
+
+  const toggleShow = useCallback((id: string) => {
+    setShown((shownId) => ({
+      ...shownId,
+      [id]: !shownId[id],
+    }))
+  }, [])
+
+  const renderShownComponent = (
+    id: string,
+    buttonText: string,
+    component: any
+  ) => {
+    return (
+      <>
+        <Button
+          onClick={() => {
+            toggleShow(id)
+          }}
+          styling={{ small: true, inverted: false }}
+        >
+          {buttonText}
+        </Button>
+        {shown[id] && component}
+      </>
+    )
+  }
 
   return (
     <>
@@ -53,31 +121,61 @@ const Performance = () => {
             Using Dynamics of NextJS, the performance logs are visible in
             console and only in DEV
           </h3>
-          <Button
-            onClick={() => {
-              setShowHeavyLoader(!showHeavyLoader)
-            }}
-            styling={{ small: true, inverted: false }}
-          >
-            Toggle Display
-          </Button>
-          {showHeavyLoader && <HeavyLoaderNoSSR />}
+          {renderShownComponent(
+            "heavyLoader",
+            "Toggle Display",
+            <HeavyLoaderNoSSR />
+          )}
         </section>
       </div>
+
       <div className={styles.container}>
         <section>
           <h3>Using React 18 Suspense</h3>
-          <Button
-            onClick={() => {
-              setShowSuspense(!showSuspense)
-            }}
-            styling={{ small: true, inverted: false }}
-          >
-            Toggle Suspense Display
-          </Button>
-          {showSuspense && <SuspenseNoSSR />}
+          {renderShownComponent(
+            "suspense",
+            "Toggle Suspense Display",
+            <SuspenseNoSSR />
+          )}
         </section>
       </div>
+
+      <div className={styles.container}>
+        <section>
+          <h3>Testing React Tracked</h3>
+
+          {renderShownComponent(
+            "reactTracked",
+            "Toggle React Tracked",
+            <ReactTrackedNoSSR />
+          )}
+        </section>
+      </div>
+
+      <div className={styles.container}>
+        <section>
+          <h3>Large Form Handling</h3>
+
+          {renderShownComponent(
+            "largeForm",
+            "Toggle Large Form",
+            <LargeFormNoSSR />
+          )}
+        </section>
+      </div>
+
+      <div className={styles.container}>
+        <section>
+          <h3>Large Form Handling with React-Tracked</h3>
+
+          {renderShownComponent(
+            "largeForm2",
+            "Toggle Large Form 2",
+            <LargeForm2NoSSR />
+          )}
+        </section>
+      </div>
+
       <Footer />
       <ScrollToTop />
     </>
