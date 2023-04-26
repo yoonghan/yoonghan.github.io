8d60a21ea3db5a0ac45b0d42d19b0d2d45205663
Han <walcoorperation@gmail.com>
2023-04-27 00:05:24 +0800

    Add webrtc project. (#162)
    * Add webrtc project.
    
    * Update backstop
    
    * Update backstop with auto approval

---
+++ src/components/VideoChat/index.tsx
@@ -0,0 +1,103 @@
+import {
+  forwardRef,
+  memo,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react"
+import styles from "./VideoChat.module.css"
+
+type Props = {
+  id: string
+  play: boolean
+  muted: boolean
+  videoFailedCallback: (exception: unknown) => void
+  videoTracksCallback: (mediaStream: MediaStream | undefined) => void
+}
+
+export interface VideoStreamHandler {
+  stream: (stream: MediaStream) => void
+}
+
+const VideoChat = forwardRef<VideoStreamHandler, Props>(
+  function VideoWithStreamHandler(
+    { id, play, muted, videoFailedCallback, videoTracksCallback }: Props,
+    ref
+  ) {
+    const videoRef = useRef<HTMLVideoElement>(null)
+    const streamRef = useRef<MediaStream>()
+
+    const stream = useCallback((stream: MediaStream) => {
+      if (videoRef.current !== null) {
+        videoRef.current.srcObject = stream
+      }
+    }, [])
+
+    useImperativeHandle(ref, () => {
+      return {
+        stream,
+      }
+    })
+
+    const gotStream = useCallback(
+      (stream: MediaStream) => {
+        if (videoRef.current !== null) {
+          videoRef.current.srcObject = stream
+          streamRef.current = stream
+          videoTracksCallback(stream)
+        }
+      },
+      [videoTracksCallback]
+    )
+
+    const startVideo = useCallback(async () => {
+      try {
+        const videoStream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        })
+        gotStream(videoStream)
+      } catch (exception) {
+        videoFailedCallback(exception)
+      }
+    }, [gotStream, videoFailedCallback])
+
+    const stopVideo = useCallback(() => {
+      if (streamRef.current && streamRef.current?.getTracks) {
+        streamRef.current.getTracks().forEach((track) => {
+          track.stop() //Comment - make sure Chrome browsers tab has no red icon/speaker when stopped
+        })
+        streamRef.current = undefined
+      }
+      if (videoRef.current !== null) {
+        videoRef.current.srcObject = null
+        videoTracksCallback(undefined)
+      }
+    }, [videoTracksCallback])
+
+    useEffect(() => {
+      if (play) {
+        startVideo()
+      } else {
+        stopVideo()
+      }
+      return stopVideo
+    }, [startVideo, stopVideo, play])
+
+    return (
+      // eslint-disable-next-line jsx-a11y/media-has-caption
+      <video
+        ref={videoRef}
+        id={id}
+        autoPlay
+        muted={muted}
+        playsInline
+        data-testid={"video-chat"}
+        className={styles.container}
+      ></video>
+    )
+  }
+)
+
+export default VideoChat
