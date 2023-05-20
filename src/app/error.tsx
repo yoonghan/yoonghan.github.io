c86372a671274ad9062f785c94e075308ca3f109
han <walcoorperation@gmail.com>
2023-05-21 07:27:27 +0800

    Restore 404 page

--- src/app/error.tsx
+++
@@ -1,5 +0,0 @@
-"use client"
-
-import NotFound from "./not-found"
-
-export default NotFound
---
+++ src/pages/404.tsx
@@ -0,0 +1,5 @@
+import NotFound from "@/app/not-found"
+
+const NotFoundReplica = NotFound
+
+export default NotFoundReplica
