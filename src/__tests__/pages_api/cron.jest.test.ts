48efcc0cd960f1b482cb572a1aa0cabe48d02dd1
han <walcoorperation@gmail.com>
2023-05-26 06:58:45 +0800

    Update cron test to remove mocking of module method

--- src/__tests__/pages_api/cron.jest.test.ts
+++ src/__tests__/pages_api/cron.jest.test.ts
@@ -1,40 +1,42 @@
 import "../../__mocks__/apiMockNext13"
+import { prismaMock } from "../../__mocks__/prismaMock"
+import { GET } from "@/app/api/cron/route"
 import { NextRequest } from "next/server"
 
 describe("cron", () => {
-  const executeFn = jest.fn()
-
-  const getGet = (() => {
-    jest.mock("@/app/api/cron/module", () => {
-      return {
-        execute: executeFn,
-      }
-    })
-    const { GET } = require("@/app/api/cron/route")
-    return GET
-  })()
-
   it("should call execute with correct query and method", async () => {
     const request = new NextRequest("http://walcron.com/api/cron", {
       method: "GET",
     })
-
-    const response = await getGet(request)
+    const historyResult = [
+      {
+        id: 1,
+        jobName: "Test CronJob 1",
+        createdAt: new Date(),
+      },
+    ]
+    prismaMock.cronJob.findMany.mockResolvedValue(historyResult)
+    const response = await GET(request)
     expect(response.status).toBe(200)
-    expect(executeFn).toHaveBeenCalled()
+    expect(await response.json()).toStrictEqual(
+      historyResult.map((it) => ({
+        ...it,
+        createdAt: it.createdAt.toISOString(),
+      }))
+    )
   })
 
   it("should call execute with error when exception is returned", async () => {
-    executeFn.mockReturnValue({
-      error: "mimic exception",
-    })
-    const request = new NextRequest("http://walcron.com/api/cron", {
-      method: "GET",
-    })
-    const response = await getGet(request)
+    const request = new NextRequest(
+      "http://walcron.com/api/cron?action=invalid",
+      {
+        method: "GET",
+      }
+    )
+    const response = await GET(request)
     expect(response.status).toBe(400)
     expect(await response.json()).toStrictEqual({
-      error: "mimic exception",
+      error: "Only GET with action query of log",
     })
   })
 })
