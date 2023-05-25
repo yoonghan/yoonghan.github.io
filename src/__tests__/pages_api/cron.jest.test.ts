33e401e5d0d92b201750d460ae5473afc027f942
han <walcoorperation@gmail.com>
2023-05-26 01:00:04 +0800

    Fix cron jest test

--- src/__tests__/pages_api/cron.jest.test.ts
+++ src/__tests__/pages_api/cron.jest.test.ts
@@ -1,36 +1,40 @@
 import "../../__mocks__/apiMockNext13"
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 
 describe("cron", () => {
-  it("should call execute with correct query and method", async () => {
-    const executeFn = jest.fn()
-    const request = new NextRequest("http://walcron.com/api/cron", {
-      method: "GET",
-    })
+  const executeFn = jest.fn()
+
+  const getGet = (() => {
     jest.mock("@/app/api/cron/module", () => {
       return {
         execute: executeFn,
       }
     })
     const { GET } = require("@/app/api/cron/route")
-    const response = await GET(request)
+    return GET
+  })()
+
+  it("should call execute with correct query and method", async () => {
+    const request = new NextRequest("http://walcron.com/api/cron", {
+      method: "GET",
+    })
+
+    const response = await getGet(request)
     expect(response.status).toBe(200)
     expect(executeFn).toHaveBeenCalled()
   })
 
   it("should call execute with error when exception is returned", async () => {
-    const executeFn = jest.fn()
-    const request = new NextRequest(
-      "http://walcron.com/api/cron?action=invalid",
-      {
-        method: "GET",
-      }
-    )
-    const { GET } = require("@/app/api/cron/route")
-    const response = await GET(request)
+    executeFn.mockReturnValue({
+      error: "mimic exception",
+    })
+    const request = new NextRequest("http://walcron.com/api/cron", {
+      method: "GET",
+    })
+    const response = await getGet(request)
     expect(response.status).toBe(400)
     expect(await response.json()).toStrictEqual({
-      error: "Only GET with action query of log",
+      error: "mimic exception",
     })
   })
 })
