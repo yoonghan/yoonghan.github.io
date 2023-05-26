5ef73530cf9d6ac0c196aae45cc3ccb2808d501b
han <walcoorperation@gmail.com>
2023-05-26 12:55:20 +0800

    fix external link

--- src/__tests__/notfound.jest.test.tsx
+++ src/__tests__/notfound.jest.test.tsx
@@ -6,7 +6,12 @@ describe("not found page", () => {
   it("should render not found page", async () => {
     render(<NotFound />)
     assertFooter()
-    expect(screen.getByText("404"))
-    expect(screen.getByText("This page is not found"))
+    expect(screen.getByText("404")).toBeDefined()
+    expect(screen.getByText("This page is not found")).toBeDefined()
+
+    //link to root has issue with NEXT
+    expect(
+      screen.getByRole("button", { name: "Go back to home" })
+    ).toHaveAttribute("href", "https://www.walcron.com")
   })
 })
