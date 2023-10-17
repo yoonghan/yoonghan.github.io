f1a54718c35e7727cdad37ce79802aaa7cc7a3a8
han <walcoorperation@gmail.com>
2023-10-17 08:07:56 +0800

    Update to display error with description.

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -1,7 +1,22 @@
-export const validInput = (value?: string) =>
-  value !== undefined &&
-  /^[a-z|A-Z|0-9|!|\$|@|?|#|%|\^]+$/.test(value) &&
-  value.length >= 6
+import {
+  is,
+  greaterThan,
+  matchesPattern,
+  not,
+  equalTo,
+  undefined,
+  allOf,
+  hasSize,
+} from "hamjest"
 
-export const validEmail = (email: string) =>
-  /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
+export const inputMatcher = allOf(
+  is(not(equalTo(undefined()))),
+  matchesPattern(/^[a-z|A-Z|0-9|!|\$|@|?|#|%|\^]+$/),
+  hasSize(greaterThan(5))
+)
+
+export const emailMatcher = matchesPattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+
+export const validInput = (value?: string) => inputMatcher.matches(value)
+
+export const validEmail = (email: string) => emailMatcher.matches(email)
