5a1a05c993e94ae5dad1c257b573f6a1ebefd5ab
Han <walcoorperation@gmail.com>
2023-11-22 12:30:39 +0800

    Add microfrontend (#202)
    * Add microfrontend
    
    * Update footer's test case.
    
    * Add testcase for microfrontend.
    
    * Fix test for footer option.
    
    * Bump revision.
    
    * Update backstop with auto approval

---
+++ src/app/projects/microfrontend/page.tsx
@@ -0,0 +1,39 @@
+import Card from "@/components/Card"
+import { memo } from "react"
+import styles from "../Projects.module.css"
+
+const Projects = ({}) => {
+  const cards = [
+    {
+      id: "microfront-end-spa",
+      title: "Microfrontend with Single Spa",
+      description:
+        "More comprehensive microfrontend with single spa. Way more complex compared to module federation.",
+      href: "https://zelda.walcron.com/",
+    },
+    {
+      id: "microfront-end-mf",
+      title: "Microfrontend with Module Federation",
+      description:
+        "First experiment with microfrontend after attending courses based on Module Federation. Won't be updated.",
+      href: "https://container.walcron.com/",
+    },
+  ]
+
+  return (
+    <div className={`${styles.container}`}>
+      <div className={`page-aligned-container`}>
+        <h1 className="title">Microfrontend</h1>
+        <div>
+          <p>
+            Partially experimental but going forward, all written javascript
+            projects are moved into Zelda.
+          </p>
+          <Card cards={cards} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default memo(Projects)
