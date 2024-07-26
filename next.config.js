a175a435edfe2a50038bf696965ee3c3d38914bf
han <walcoorperation@gmail.com>
2024-07-26 16:57:36 +0800

    Remove next-pwa

--- next.config.js
+++ next.config.js
@@ -25,21 +25,13 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
 
-const withPWA = require("next-pwa")({
-  disable: process.env.NODE_ENV === "development",
-  dest: "public",
-  register: false,
+module.exports = withBundleAnalyzer({
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ]
+  },
 })
-
-module.exports = withBundleAnalyzer(
-  withPWA({
-    async headers() {
-      return [
-        {
-          source: "/:path*",
-          headers: securityHeaders,
-        },
-      ]
-    },
-  })
-)
