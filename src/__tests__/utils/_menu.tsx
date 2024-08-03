4ff81eb2fd0d5830d395247bf98d0e28c2e3098a
han <walcoorperation@gmail.com>
2024-08-04 07:22:10 +0800

    Merge tests utils.

--- src/__tests__/utils/_menu.tsx
+++
@@ -1,6 +0,0 @@
-import { screen } from "@testing-library/react"
-
-export const assertMenu = async () => {
-  expect(screen.getByRole("img", { name: "home" })).toBeInTheDocument()
-  expect(await screen.findAllByText("walcron@tm$")).toHaveLength(2)
-}
--- src/__tests__/utils/_socialFab.tsx
+++
@@ -1,5 +0,0 @@
-import { screen } from "@testing-library/react"
-
-export const assertSocialFab = async () => {
-  expect(await screen.findByRole("link", { name: "git" })).toBeInTheDocument()
-}
