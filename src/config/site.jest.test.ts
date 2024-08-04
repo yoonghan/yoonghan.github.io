cf0a9ddf6c0372c6d3ef70f91a9525a18dbbb4f2
han <walcoorperation@gmail.com>
2024-08-04 15:05:23 +0800

    Introduce site api url

--- src/config/site.jest.test.ts
+++ src/config/site.jest.test.ts
@@ -1,15 +1,23 @@
 import { setEnv } from "@/__tests__/mocks/setEnv"
 
 describe("root-url", () => {
-  it("should be default as blank", async () => {
+  it("should be default with walcron", async () => {
     const { site } = await import("./site")
     expect(site.url).toBe("https://www.walcron.com")
+    expect(site.apiUrl).toBe("https://www.walcron.com/api")
   })
 
-  it("should be fixed as https://www.walcron.com", async () => {
+  it("should getUrl from env", async () => {
     const url = "https://yoonghan.github.io"
     setEnv({ SITE_URL: url })
-    const { getSiteUrl } = await import("./site")
-    expect(getSiteUrl()).toBe(url)
+    const { getUrl } = await import("./site")
+    expect(getUrl()).toBe(url)
+  })
+
+  it("should getUrl + getApiUrl from env", async () => {
+    const url = "https://yoonghan.github.io"
+    setEnv({ SITE_URL: url })
+    const { getApiUrl } = await import("./site")
+    expect(getApiUrl()).toBe(url + "/api")
   })
 })
