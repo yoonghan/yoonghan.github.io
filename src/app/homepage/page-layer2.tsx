7a71562952b375f741b9968fed47889f9f73eb67
han <walcoorperation@gmail.com>
2024-08-05 16:08:50 +0800

    Use new cookie

--- src/app/homepage/page-layer2.tsx
+++ src/app/homepage/page-layer2.tsx
@@ -1,33 +1,8 @@
 "use client"
-
-import { useEffect, useState } from "react"
-import { updateCookie } from "./cookie-util"
 import Homepage from "./main"
 
 const IndexLayer2 = () => {
-  const [isCookieRead, setCookieRead] = useState(false)
-
-  useEffect(() => {
-    function getCookie(cname: string) {
-      let name = cname + "="
-      let decodedCookie = decodeURIComponent(document.cookie)
-      let ca = decodedCookie.split(";")
-      for (let i = 0; i < ca.length; i++) {
-        let c = ca[i]
-        while (c.charAt(0) == " ") {
-          c = c.substring(1)
-        }
-        if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length)
-        }
-      }
-      return ""
-    }
-    setCookieRead(!!getCookie("termsRead"))
-    updateCookie()
-  }, [])
-
-  return <Homepage termsRead={isCookieRead} />
+  return <Homepage />
 }
 
 export default IndexLayer2
