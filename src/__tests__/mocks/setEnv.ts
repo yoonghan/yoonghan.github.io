73a938467060cbd91d6483abc6764e41de05e23d
han <walcoorperation@gmail.com>
2024-08-10 06:47:02 +0800

    Override api url

--- src/__tests__/mocks/setEnv.ts
+++ src/__tests__/mocks/setEnv.ts
@@ -1,6 +1,6 @@
 const defaultProcessEnv = { ...process.env }
 
-export const setEnv = (environments: { [key: string]: string }) => {
+export const setEnv = (environments: { [key: string]: string | undefined }) => {
   Object.keys(environments).forEach((key: string) => {
     process.env[key] = environments[key]
   })
