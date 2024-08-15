3021da49c3f5a7ea03b72e96a492e461eccea6f0
han <walcoorperation@gmail.com>
2024-08-15 09:08:05 +0800

    Improve homepage grammars

--- src/app/homepage/index.tsx
+++ src/app/homepage/index.tsx
@@ -6,6 +6,7 @@ import Figure from "@/components/Figure"
 
 function Homepage() {
   const walcronText = <strong>Walcron</strong>
+  const quoteText = (text: string) => <>&quot;{text}&quot;</>
 
   return (
     <>
@@ -21,11 +22,6 @@ function Homepage() {
             We&apos;re just a regular coder who are keen in{" "}
             <strong>Web and Cloud</strong> technologies.
           </p>
-
-          <p>
-            This site to <strong>records</strong> our development and things we
-            learn.
-          </p>
         </article>
         <hr />
         <article className="max-w-screen-md mx-auto">
@@ -39,8 +35,8 @@ function Homepage() {
             <strong>Non-Javascript Accessible</strong> site.
           </p>
           <p>
-            In summary as of <strong>2024</strong>, the importance of{" "}
-            {walcronText} website are:
+            As of <strong>2024</strong>, the importance of {walcronText} website
+            are:
           </p>
           <nav>
             <ul className="text-left -mt-6 ml-4 list-decimal">
@@ -70,9 +66,11 @@ function Homepage() {
           <div className="text-center my-8">
             Being a self-maintained site, how do one...
             <ul className="text-lg italic pt-4">
-              <li>&quot;Validate his own changes ?&quot;</li>
-              <li>&quot;Ensure changes doesn&apos;t break anything ?&quot;</li>
-              <li>&quot;How to do frequent deployment ?&quot;</li>
+              <li>{quoteText("Validate his own changes ?")}</li>
+              <li>
+                {quoteText("Ensure changes doesn&apos;t break anything ?")}
+              </li>
+              <li>{quoteText("A hassle to deploy changes ?")}</li>
             </ul>
           </div>
           <section className="max-w-screen-md mx-auto">
@@ -91,56 +89,49 @@ function Homepage() {
                   <Link href="https://docs.github.com/en/actions/writing-workflows">
                     GitHub workflow
                   </Link>{" "}
-                  to do our biddings. To do our continous integration and
-                  deployment (CI/CD):
+                  to do our biddings. A full CI/CD:
                   <ul className="mt-4 ml-4 list-disc">
                     <li>
-                      <strong>Testing</strong> - nothing breaks with new
-                      changes. [
+                      <strong>Testing</strong> - ensure safety in modification.
+                      [
                       <Link href="https://github.com/yoonghan/yoonghan.github.io/actions/workflows/push.yml?query=workflow%3A">
                         GitHub Actions
                       </Link>
                       ]
                     </li>
                     <li>
-                      <strong>Coverage</strong> - not slacking at 99.9%
-                      coverage.[
+                      <strong>Coverage</strong> - 99.9% lines of code covered.[
                       <Link href="https://app.codecov.io/gh/yoonghan/yoonghan.github.io">
-                        Tracked In CodeCov
+                        CodeCov
                       </Link>
                       ]
                     </li>
                     <li>
-                      <strong>Integration</strong> - api with external party
-                      always worked. [
+                      <strong>Integration</strong> - API doesn&apos;t break. [
                       <Link href="https://github.com/yoonghan/yoonghan.github.io/actions/workflows/pull-request.yml">
-                        Smoke test and E2E test
+                        Smoke and E2E
                       </Link>
                       ]
                     </li>
                     <li>
-                      <strong>Layout verification</strong> - workflow to approve
-                      User Interface changes. [
+                      <strong>Layout verification</strong> - Workflow on User
+                      Interface changes. [
                       <Link href="https://github.com/yoonghan/yoonghan.github.io/actions/runs/10380878898">
                         Snapshots
                       </Link>
                       ]
                     </li>
                     <li>
-                      <strong>Compliance</strong> - Security and coding
-                      standards. [
+                      <strong>Compliance</strong> - Security and Lint checks. [
                       <Link href="https://github.com/yoonghan/yoonghan.github.io/actions/workflows/push.yml">
-                        Owasp check
+                        OWASP check
                       </Link>{" "}
                       and Dependabots]
                     </li>
                     <li>
-                      <strong>Restriction</strong> - Master deployment branch is
-                      protected and allow only{" "}
-                      <em className="text-emerald-500">
-                        successful pull-request
-                      </em>{" "}
-                      to deploy.
+                      <strong>Restriction</strong> - Deploys ONLY{" "}
+                      <em className="text-emerald-500">ONLY successful</em>{" "}
+                      pull-request.
                     </li>
                   </ul>
                 </>
@@ -148,7 +139,7 @@ function Homepage() {
             </section>
             <section className="max-w-screen-md mx-auto pt-8">
               <div className="text-center text-lg mb-12 italic">
-                &quot;So what if something really breaks ?&quot;
+                {quoteText("So what if something really breaks ?")}
               </div>
               <section>
                 <Figure
@@ -162,8 +153,8 @@ function Homepage() {
                   reversed={true}
                 >
                   <>
-                    <strong>LOL!</strong> In reality this will happen. There are
-                    fallbacks options to this:
+                    In reality this will happen, and there are fallbacks options
+                    :
                     <ul className="list-decimal ml-4 mt-4">
                       <li>
                         Every pull-request is tracked via Release [
@@ -175,7 +166,8 @@ function Homepage() {
                       </li>
                       <li>
                         Revert via <Link href="https://vercel.com">Vercel</Link>
-                        , but will not work for GitHub Pages.
+                        , but not applicable to GitHub Pages. Useful for API
+                        break changes.
                       </li>
                     </ul>
                   </>
@@ -197,15 +189,18 @@ function Homepage() {
             </div>
             <p className="pt-8">
               We did it by relying on both PureCSS, Server Side Generation and
-              ensuring non-critical usage for Javascript. The old home was{" "}
-              <Link href="/experiments/homepage-v1">Parallax heavy</Link> and we
-              encountered performance + accessibility issues.
+              ensuring non-critical usage for Javascript. The old homepage was{" "}
+              <Link href="/experiments/homepage-v1">Javascript heavy</Link>
+              (Parallax Effect) and we encountered{" "}
+              <span className="underline">performance</span> and{" "}
+              <span className="underline">accessibility</span>
+              issues.
             </p>
           </section>
 
           <section className="pt-8">
             <div className="text-xl italic text-center">
-              This site can also be browse with keyboard and text-readers.
+              This site can also be browsed with keyboard and text-readers.
             </div>
             <p className="pt-8">
               We are going now commited with this Web Accessibility; eventhough
