f4c50b6780779f34db520d630373c17cf829342c
han <walcoorperation@gmail.com>
2024-08-15 12:32:17 +0800

    Add testcase and adjust layout based on test

--- src/app/homepage/index.jest.test.tsx
+++ src/app/homepage/index.jest.test.tsx
@@ -42,4 +42,52 @@ describe("HomePage", () => {
       )
     })
   })
+
+  describe("accessibility section", () => {
+    it("can navigate for accessibility", () => {
+      const title = "Accessibility and Readability (WCAG)"
+      const link = "accessibility"
+      renderComponent()
+      expect(screen.getByRole("link", { name: title })).toHaveAttribute(
+        "href",
+        `#${link}`
+      )
+      expect(screen.getByRole("heading", { name: title })).toHaveAttribute(
+        "id",
+        link
+      )
+    })
+  })
+
+  describe("monitoring section", () => {
+    it("can navigate for monitoring", () => {
+      const title = "Monitoring"
+      const link = "monitoring"
+      renderComponent()
+      expect(screen.getByRole("link", { name: title })).toHaveAttribute(
+        "href",
+        `#${link}`
+      )
+      expect(screen.getByRole("heading", { name: title })).toHaveAttribute(
+        "id",
+        link
+      )
+    })
+  })
+
+  describe("performance section", () => {
+    it("can navigate for performance", () => {
+      const title = "Performance"
+      const link = "performance"
+      renderComponent()
+      expect(screen.getByRole("link", { name: title })).toHaveAttribute(
+        "href",
+        `#${link}`
+      )
+      expect(screen.getByRole("heading", { name: title })).toHaveAttribute(
+        "id",
+        link
+      )
+    })
+  })
 })
