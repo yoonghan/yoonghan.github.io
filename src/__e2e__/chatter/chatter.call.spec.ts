cb1270359d9ff584d98800e4375d8246ac69171d
han <walcoorperation@gmail.com>
2024-08-17 05:12:48 +0800

    Add console.

--- src/__e2e__/chatter/chatter.call.spec.ts
+++ src/__e2e__/chatter/chatter.call.spec.ts
@@ -1,8 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable testing-library/prefer-screen-queries */
+
 /*
- Do not split test, pusher has issue if not disconnected, next start would not find proper users.
+ Do not split test.
+ Due to be using vercel preview, many instabilities happen over here.
 */
 
-/* eslint-disable testing-library/prefer-screen-queries */
 import { test, expect } from "@playwright/test"
 import { callAnotherPerson, startCall } from "./call-util"
 
@@ -10,8 +13,10 @@ test.describe("Webrtc calls", () => {
   test("should be able to start 2 persons call, and receiver rejects it", async () => {
     const caller = await startCall("Jessica")
     const receiver = await startCall("Michelle")
+    console.log("Reject startup")
 
     await callAnotherPerson(caller, receiver, "Jessica", "Michelle")
+    console.log("Reject call")
 
     await receiver.getByRole("button", { name: "No" }).click({ force: true })
     await caller.waitForTimeout(1000)
@@ -19,12 +24,15 @@ test.describe("Webrtc calls", () => {
     expect(await caller.content()).toContain(
       "Call to (Michelle) was politely declined."
     )
+    console.log("Reject decline")
+
     await caller.getByRole("button", { name: "Ok" }).click({ force: true })
     await caller.waitForTimeout(1000)
 
     await receiver.getByRole("button", { name: "Stop" }).click({ force: true })
     await caller.getByRole("button", { name: "Stop" }).click({ force: true })
 
+    console.log("Reject call stop")
     await caller.close()
     await receiver.close()
   })
@@ -32,14 +40,18 @@ test.describe("Webrtc calls", () => {
   test("should be able to start 2 persons call, and receiver answers it", async () => {
     const caller = await startCall("Jupiter")
     const receiver = await startCall("Mars")
+    console.log("Accept startup")
 
     await callAnotherPerson(caller, receiver, "Jupiter", "Mars")
+    console.log("Accept call")
 
     await receiver.getByRole("button", { name: "Yes" }).click({ force: true })
     await caller.waitForTimeout(1000)
+    console.log("Accept confirm")
 
     await caller.getByRole("button", { name: "Stop" }).click({ force: true })
     await caller.waitForTimeout(1000)
+    console.log("Accept decline")
 
     expect(await receiver.content()).toContain(
       "User (Jupiter) has left the chat."
@@ -47,6 +59,7 @@ test.describe("Webrtc calls", () => {
 
     await receiver.getByRole("button", { name: "Ok" }).click({ force: true })
     await receiver.waitForTimeout(1000)
+    console.log("Accept done")
 
     await caller.close()
     await receiver.close()
