8fc3d476952b3c7281dc98c751d535128f332f28
han <walcoorperation@gmail.com>
2024-08-21 14:35:37 +0800

    Add canonical site reference.

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,10 +1,14 @@
 import Script from "next/script"
 import { schema } from "./config/schema"
+import { site } from "@/config/site"
 
 export const metadata = {
   title: "About Walcron",
   description:
     "Walcron Coorperation is a basic company setup by Yoong Han and Lee Wan for World Wide Web research purposes.",
+  alternates: {
+    ...site.generateCanonical("/about"),
+  },
 }
 
 export default function AboutLayout({
--- src/app/projects/javascript-free/layout.tsx
+++ src/app/projects/javascript-free/layout.tsx
@@ -1,6 +1,11 @@
+import { site } from "@/config/site"
+
 export const metadata = {
   title: "Javascript free",
   description: "Playground for components that can work without Javascript.",
+  alternates: {
+    ...site.generateCanonical("/projects/javascript-free"),
+  },
 }
 
 export default function JsFreedom({ children }: { children: React.ReactNode }) {
