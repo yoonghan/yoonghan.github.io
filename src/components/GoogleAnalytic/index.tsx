c3aff1c1b8990729db65b02a18951e1f99dccb1e
han <walcoorperation@gmail.com>
2024-08-25 09:51:38 +0800

    Send 2 different webvital events.

--- src/components/GoogleAnalytic/index.tsx
+++ src/components/GoogleAnalytic/index.tsx
@@ -13,12 +13,19 @@ export function GoogleAnalytic({ ga4Id }: Props) {
   ReactGA.initialize(ga4Id)
 
   //https://pilotdigital.com/blog/how-to-monitor-core-web-vitals-in-google-analytics/
+  //https://github.com/google-marketing-solutions/web-vitals-gtm-template
   useEffect(() => {
-    reportWebVitals(({ name, delta, id }) => {
+    reportWebVitals(({ name, delta, id, value }) => {
       ReactGA.event("web_vitals", {
         cwv_metric: name,
         cwv_value: delta,
         cwv_id: id,
+      })
+      ReactGA.gtag("event", name, {
+        value: delta,
+        metric_id: id,
+        metric_value: value,
+        metric_delta: delta,
       })
     })
   }, [])
