443ad67bd33243ee5b118b6b4b03c13e4b673089
han <walcoorperation@gmail.com>
2024-08-29 22:50:45 +0800

    Split server configuration to enable SSG.

--- next.config.js
+++ next.config.js
@@ -1,43 +1,3 @@
-const securityHeaders = [
-  {
-    key: "X-DNS-Prefetch-Control",
-    value: "on",
-  },
-  {
-    key: "Strict-Transport-Security",
-    value: "max-age=63072000; includeSubDomains; preload",
-  },
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-  {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
-  },
-]
-
-const apiSecurityHeaders = [
-  { key: "Access-Control-Allow-Credentials", value: "true" },
-  {
-    key: "Access-Control-Allow-Origin",
-    value: process.env.CORS_ALLOW_ORIGIN || "localhost",
-  },
-  {
-    key: "Access-Control-Allow-Methods",
-    value: "GET,DELETE,PATCH,POST,PUT",
-  },
-  {
-    key: "Access-Control-Allow-Headers",
-    value:
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-  },
-]
-
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({
 //   enabled: true,
 // })
@@ -48,17 +8,4 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
 })
 
-module.exports = withPWA({
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: securityHeaders,
-      },
-      {
-        source: "/api/:path*",
-        headers: apiSecurityHeaders,
-      },
-    ]
-  },
-})
+module.exports = withPWA(require("./extended.next.config.js"))
