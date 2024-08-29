35960a46c64ee7baf479e14f29b30ff3e77de6a3
han <walcoorperation@gmail.com>
2024-08-29 16:26:13 +0800

    Reduce google analytic called.

--- src/components/GoogleAnalytic/index.tsx
+++ src/components/GoogleAnalytic/index.tsx
@@ -3,13 +3,16 @@
 import ReactGA from "react-ga4"
 import { reportWebVitals } from "@yoonghan/walcron-microfrontend-shared"
 import ClientCookie from "./ClientCookie"
+import { useEffect } from "react"
 
 interface Props {
   ga4Id: string
 }
 
 export function GoogleAnalytic({ ga4Id }: Props) {
-  ReactGA.initialize(ga4Id)
+  useEffect(() => {
+    ReactGA.initialize(ga4Id)
+  }, [ga4Id])
 
   reportWebVitals(({ name, delta, id, value }) => {
     ReactGA.event(name, {
