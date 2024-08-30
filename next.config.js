c49d220b4c2df348e36004b1bf923a1bbe82cbde
han <walcoorperation@gmail.com>
2024-08-30 17:00:17 +0800

    Reconfigure exclude that need to include !

--- next.config.js
+++ next.config.js
@@ -46,7 +46,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   disable: process.env.NODE_ENV === "development",
   register: false,
   dest: "public",
-  publicExcludes: ["**/*.mp4", "**/*.mp3", "**/*.jpg", "**/*.png"],
+  publicExcludes: ["!**/*.mp4", "!**/*.mp3", "!**/*.png"],
 })
 
 module.exports = withPWA({
