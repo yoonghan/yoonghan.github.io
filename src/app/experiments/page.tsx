3dab20a04d96580e3fd3051c8d92abfe1b5f6637
han <walcoorperation@gmail.com>
2024-09-11 10:38:42 +0800

    Remove text color

--- src/app/experiments/page.tsx
+++ src/app/experiments/page.tsx
@@ -1,7 +1,6 @@
 import Card from "@/components/Card"
 import { memo } from "react"
 import ScrollToTop from "@/components/ScrollToTop"
-import styles from "./Experiments.module.css"
 import LetterBox from "@/components/LetterBox"
 import { site } from "@/config/site"
 
@@ -15,52 +14,50 @@ export const metadata = {
 
 const Experiments = ({}) => {
   return (
-    <>
-      <div className={`${styles.container}`}>
-        <div className="mx-auto max-w-screen-lg px-4 pb-8">
-          <h1>Experimental projects</h1>
-          <div>
-            <p>
-              Incomplete or projects that are used for test-beds
-              <small>(due to migration most are not moved over)</small>
-            </p>
-            <Card
-              cards={[
-                {
-                  id: "amp",
-                  title: "Accelerated Mobile Pages",
-                  description: "Google's approach of fast mobile page.",
-                  href: "/experiments/amp",
-                  target: "_self",
-                },
-                {
-                  id: "storybook",
-                  title: "Storybook",
-                  description: "Design and styling of UX/UI of Walcron page.",
-                  href: "/experiments/storybook",
-                  target: "_self",
-                },
-                {
-                  id: "performance",
-                  title: "Performance",
-                  description: "Validate react performance",
-                  href: "/experiments/performance",
-                  target: "_self",
-                },
-              ]}
-            />
-          </div>
+    <div className="p-8">
+      <div className="mx-auto max-w-screen-lg px-4 pb-8">
+        <h1>Experimental projects</h1>
+        <div>
+          <p>
+            Incomplete or projects that are used for test-beds
+            <small>(due to migration most are not moved over)</small>
+          </p>
+          <Card
+            cards={[
+              {
+                id: "amp",
+                title: "Accelerated Mobile Pages",
+                description: "Google's approach of fast mobile page.",
+                href: "/experiments/amp",
+                target: "_self",
+              },
+              {
+                id: "storybook",
+                title: "Storybook",
+                description: "Design and styling of UX/UI of Walcron page.",
+                href: "/experiments/storybook",
+                target: "_self",
+              },
+              {
+                id: "performance",
+                title: "Performance",
+                description: "Validate react performance",
+                href: "/experiments/performance",
+                target: "_self",
+              },
+            ]}
+          />
         </div>
-        <hr />
-        <section>
-          <h2>Contact</h2>
-          <div className="p-padding center">
-            <LetterBox />
-          </div>
-        </section>
       </div>
+      <hr />
+      <section>
+        <h2>Contact</h2>
+        <div className="p-padding center">
+          <LetterBox />
+        </div>
+      </section>
       <ScrollToTop />
-    </>
+    </div>
   )
 }
 
