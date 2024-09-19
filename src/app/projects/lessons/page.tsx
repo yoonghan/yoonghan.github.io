4aceebfcaede8bc550f619de0d8439fb267e1241
Yoong Han <walcoorperation@gmail.com>
2024-09-19 15:08:12 +0800

    Update Lessons with ci/cds.

--- src/app/projects/lessons/page.tsx
+++ src/app/projects/lessons/page.tsx
@@ -9,6 +9,7 @@ import imgAnalyzeReportSize from "@/images/lesson/1/analyze-report-size.jpg"
 import imgTypescriptImport from "@/images/lesson/1/typescript-import.jpg"
 import imgFinalResult from "@/images/lesson/1/final-result.jpg"
 import { site } from "@/config/site"
+import Link from "@/components/Link"
 
 export const metadata = {
   title: "Lessons",
@@ -281,6 +282,25 @@ const Lessons = ({}) => {
             </ul>
           </li>
         </ul>
+      </article>
+      <article className="my-8">
+        <h3>CI/CD Caveat</h3>
+        <section>
+          <title>Gitlab</title>
+          <ul>
+            <li>Variables can only be passed to Downstream triggers with <Link href="https://docs.gitlab.com/ee/ci/pipelines/downstream_pipelines.html#pass-cicd-variables-to-a-downstream-pipeline">Variables/Artifacts and needs</Link>. Do not use Needs, it was so hard to troubleshoot.</li>
+            <li>To share variables from different stages, create a dotenv artifact (similar to Github). Variables will be pass only if 1) Following stages with no needs command, 2) Stages that refer to the dotenv stage. </li>
+            <li>{'"Needs"'} meant it can skip stages, if there is variable required from dotenv stage; include that stage into needs. i.e. needs: [stage_that_creates_dotenv, other_stages] </li>
+            <li>before_script and script are actually grouped together, after_script is special even if it fails it will not fail the stage.</li>
+          </ul>
+        </section>
+        <section>
+          <title>Github</title>
+          <ul>
+            <li>If one is creating actions, remember to add {'"shell: bash"'} for jobs running scripts.</li>
+            <li>Remember to use {'"secrets: inherit"'} on parent job to enable secret passing to downstream workflow.</li>
+          </ul>
+        </section>
       </article>
       <ScrollToTop />
     </div>
