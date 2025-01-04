c84ab0dcf50c6ba5d39a8562b359c41d1e2da2a9
han <walcoorperation@gmail.com>
2025-01-05 02:26:23 +0800

    Add Developer associate certificate.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -126,14 +126,21 @@ const About = () => {
                     "https://d1.awsstatic.com/certification/badges/AWS-Certified-Solutions-Architect-Associate_badge_150x150.e359ae4a6d4d82c3e31d4f9104c8d389b56a2423.png",
                   className: "bg-orange-400",
                   title: "AWS Solution Architect Associate",
-                  href: "https://www.credly.com/badges/964f2164-02ab-469c-b964-a01c2210ef24/public_url",
+                  href: "https://www.credly.com/badges/964f2164-02ab-469c-b964-a01c2210ef24",
+                },
+                {
+                  imageUrl:
+                    "https://d1.awsstatic.com/training-and-certification/certification-badges/AWS-Certified-Developer-Associate_badge.5c083fa855fe82c1cf2d0c8b883c265ec72a17c0.png",
+                  className: "bg-blue-400",
+                  title: "AWS Developer Associate",
+                  href: "https://www.credly.com/badges/fb17e291-5f18-4c0f-9474-364d82549bbe",
                 },
                 {
                   imageUrl:
                     "https://d1.awsstatic.com/certification/badges/AWS-Certified-Cloud-Practitioner_badge_150x150.17da917fbddc5383838d9f8209d2030c8d99f31e.png",
                   className: "bg-yellow-400",
                   title: "AWS Cloud Practitioner",
-                  href: "https://www.credly.com/badges/b17e3872-c5c5-49f7-8c6a-186e107a28d2/public-url",
+                  href: "https://www.credly.com/badges/b17e3872-c5c5-49f7-8c6a-186e107a28d2",
                 },
                 {
                   className: "bg-teal-400",
