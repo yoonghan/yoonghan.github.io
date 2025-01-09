b72075c05a72e26ffb4301e8813d102be390c9c9
han <walcoorperation@gmail.com>
2025-01-09 16:40:04 +0800

    Add opentelemetry

---
+++ instrumentation.ts
@@ -0,0 +1,36 @@
+import { registerOTel } from "@vercel/otel"
+import { SamplingDecision } from "@opentelemetry/sdk-trace-base"
+import { trace, Context } from "@opentelemetry/api"
+
+export function register() {
+  registerOTel({
+    serviceName: "acme-next-app",
+    traceExporter: "auto",
+    spanProcessors: ["auto"],
+    traceSampler: {
+      shouldSample: (context: Context) => {
+        const isChecklySpan = trace
+          .getSpan(context)
+          ?.spanContext()
+          ?.traceState?.get("checkly")
+        if (isChecklySpan) {
+          console.log(
+            "Sampling decision for Checkly span:",
+            SamplingDecision.RECORD_AND_SAMPLED
+          )
+          return {
+            decision: SamplingDecision.RECORD_AND_SAMPLED,
+          }
+        } else {
+          console.log(
+            "Sampling decision for non-Checkly span:",
+            SamplingDecision.NOT_RECORD
+          )
+          return {
+            decision: SamplingDecision.NOT_RECORD,
+          }
+        }
+      },
+    },
+  })
+}
