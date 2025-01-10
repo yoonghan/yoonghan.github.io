0668fbadff513a9b893b8a07ac91a8d4938db32d
han <walcoorperation@gmail.com>
2025-01-10 12:45:32 +0800

    Restore with PWA and move instrumentation hook to the top.

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -13,11 +13,22 @@ export function register() {
           .getSpan(context)
           ?.spanContext()
           ?.traceState?.get("checkly")
+
         if (isChecklySpan) {
+          // eslint-disable-next-line no-console
+          console.log(
+            "Sampling decision for Checkly span:",
+            SamplingDecision.RECORD_AND_SAMPLED
+          )
           return {
             decision: SamplingDecision.RECORD_AND_SAMPLED,
           }
         } else {
+          // eslint-disable-next-line no-console
+          console.log(
+            "Sampling decision for non-Checkly span:",
+            SamplingDecision.NOT_RECORD
+          )
           return {
             decision: SamplingDecision.NOT_RECORD,
           }
