3046d82059d53bc4ac0345cd0942e9efcd639e0a
han <walcoorperation@gmail.com>
2025-01-27 18:00:56 +0800

    Fix workflow testcase

--- src/components/Animate/Workflow/Workflow.jest.test.tsx
+++ src/components/Animate/Workflow/Workflow.jest.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, within } from "@testing-library/react"
 import Workflow from "."
 
 describe("Animate Workflow", () => {
@@ -10,7 +10,18 @@ describe("Animate Workflow", () => {
     )
 
   it("should render with optional className", () => {
-    renderComponent("arrow-zoom", "class-1")
-    expect(screen.getByTitle("arrow-zoom")).toHaveClass("class-1")
+    renderComponent("workflow", "class-1")
+    expect(screen.getByTitle("workflow")).toHaveClass("class-1")
+  })
+
+  it("should be able to disable animation", () => {
+    Object.defineProperty(window, "location", {
+      value: { search: "?animate=none" },
+      writable: true,
+    })
+    renderComponent("workflow-1")
+    expect(
+      within(screen.getByTitle("workflow-1")).getByTestId("workflow")
+    ).toHaveClass("animate-none")
   })
 })
