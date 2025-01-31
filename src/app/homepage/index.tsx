e11cc8c8878b2bc4cf3947bfdb1beaed8b8109be
Yoong Han <walcoorperation@gmail.com>
2025-01-31 18:28:08 +0800

    Fix styling.

--- src/app/homepage/index.tsx
+++ src/app/homepage/index.tsx
@@ -393,7 +393,9 @@ function Homepage() {
               }}
               imageCaption="Blocks and monitor Attacks with Cloudflare"
             >
-              {"Our website and host request are filtered and securely protected with Cloudflare's Web Application Firewall. On average, this website is bombarded 20 times a day."}
+              {
+                "Our website and host request are filtered and securely protected with Cloudflare's Web Application Firewall. On average, this website is bombarded 20 times a day."
+              }
             </Figure>
           </div>
         </article>
