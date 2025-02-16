e80b0d43d1b1c4c5998f2c282b9f6eb31ee34b04
han <walcoorperation@gmail.com>
2025-02-17 06:46:43 +0800

    Add funky scroller

---
+++ src/components/utils/hooks/tracker/useTrackReducer.ts
@@ -0,0 +1,52 @@
+import { debounce } from "@yoonghan/walcron-microfrontend-shared"
+import { useCallback, useReducer, useRef } from "react"
+
+enum Actions {
+  APPEND,
+}
+
+interface Action {
+  type: Actions
+  value: number
+  max: number
+}
+
+function reducer(state: number[], action: Action) {
+  const { value, type, max } = action
+
+  switch (type) {
+    case Actions.APPEND:
+      return [...(state.length === max ? state.slice(1, max) : state), value]
+  }
+}
+
+export const useTrackReducer = ({
+  initialData = [],
+  maxStorage = 10,
+  allowStorageAfterMiliseconds = 20,
+}: {
+  initialData?: number[]
+  maxStorage?: number
+  allowStorageAfterMiliseconds?: number
+}) => {
+  var canStore = useRef(true)
+  const [data, dispatch] = useReducer(reducer, initialData)
+  const append = useCallback(
+    (value: number) => {
+      if (canStore.current) {
+        dispatch({ type: Actions.APPEND, value, max: maxStorage })
+        canStore.current = false
+
+        setTimeout(() => {
+          canStore.current = true
+        }, allowStorageAfterMiliseconds)
+      }
+    },
+    [allowStorageAfterMiliseconds, maxStorage]
+  )
+
+  return {
+    data,
+    append,
+  }
+}
