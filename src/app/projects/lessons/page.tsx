0aee5698bdfc1228f6df0ce78e023b5c8f18ef78
han <walcoorperation@gmail.com>
2025-04-27 06:58:51 +0800

    Change domain transfer section.

--- src/app/projects/lessons/page.tsx
+++ src/app/projects/lessons/page.tsx
@@ -346,6 +346,56 @@ const Lessons = ({}) => {
           </ul>
         </section>
       </article>
+      <article className="my-8">
+        <strong>Domain Transfer from GoDaddy to Cloudflare</strong>
+        <p className="pt-2">
+          Had been a loyal customer of GoDaddy for 10 years, but price charges
+          from them was insanely high - USD65 per year?!
+        </p>
+        <p>
+          Decided to move to Cloudflare which was only USD10/per year for the
+          domain.
+        </p>
+
+        <ol className="list-decimal ml-4 mt-4">
+          <li>
+            Had change GoDaddy NS to Cloudflare and setup the domain here for
+            quite some years.
+          </li>
+          <li>
+            Login to Cloudflare and double check if walcron.com is listed in
+            Domain Registration.
+          </li>
+          <li>
+            Login to GoDaddy and make sure domainproxy email is forwarded.
+          </li>
+          <li>
+            Initiate transfer from GoDaddy, then an authorization Code was
+            emailed.
+          </li>
+          <li>
+            Check the domain is unlocked in GoDaddy and browse WHOIS for
+            walcron.com domain and make sure it is &quot;Domain Status: ok
+            http://icann.org/epp#ok&quot;
+          </li>
+          <li>
+            Goto Domain Transfer in Cloudflare, and fingers crossed it allows
+            transfer. Might take up to a day.
+          </li>
+          <li>
+            Trigger the transfer in Cloudflare and enter the Authorization Code.
+          </li>
+          <li>
+            Fill-in and the form to Initiate a transfer. Once done, I received
+            an email from GoDaddy.
+          </li>
+          <li>Follow the email of GoDaddy to approve the transfer</li>
+          <li>
+            WALA! All done. Best checked once more in Cloudflare and WHOIS to
+            make sure domain is locked and secured.
+          </li>
+        </ol>
+      </article>
       <ScrollToTop />
     </div>
   )
