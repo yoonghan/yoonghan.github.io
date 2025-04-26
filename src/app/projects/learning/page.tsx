da789ab779c7afb577ee9e5137a1cf1fa6a139a6
han <walcoorperation@gmail.com>
2025-04-27 06:45:20 +0800

    Add domain transfer experience.

--- src/app/projects/learning/page.tsx
+++ src/app/projects/learning/page.tsx
@@ -6,6 +6,7 @@ import { AwsCloudPractitionerArticle } from "./config/AwsCloudPractitionerArticl
 import { SunJavaDeveloperArticle } from "./config/SunJavaDeveloperArticle"
 import { ScalaFunctionalProgrammingArticle } from "./config/ScalaFunctionalProgrammingArticle"
 import { AwsSolutionArchitectArticle } from "./config/AwsSolutionArchitectArticle"
+import { DomainTransfer } from "./config/DomainTransfer"
 
 export const metadata = {
   title: "Learning",
@@ -20,6 +21,7 @@ const Learning = ({}) => {
     <div className="walcron-container">
       <h1>Certification taken</h1>
       <section className="grid grid-column gap-4">
+        <DomainTransfer />
         <AwsSolutionArchitectArticle />
         <AwsCloudPractitionerArticle />
         <SunJavaDeveloperArticle />
