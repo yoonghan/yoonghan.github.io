bae039645568d7a48ad5286ffc264da4443861b1
yoonghan <b_wars@yahoo.com>
2025-05-20 16:33:55 +0800

    Refactor for coding standard

--- src/components/Animate/Gauge/index.tsx
+++ src/components/Animate/Gauge/index.tsx
@@ -7,15 +7,15 @@ import styles from "./Gauge.module.css"
 function Gauge({
   children,
   title,
-  className,
-}: {
+  className = "",
+}: Readonly<{
   children: ReactNode
   title: string
   className?: string
-}) {
+}>) {
   const { isAnimatable } = useDisableAnimation()
   return (
-    <div className={`${className || ""}`} title={title}>
+    <div className={className} title={title}>
       <div className={styles.container}>
         <div
           className={`${styles.percent} ${isAnimatable ? "" : "animate-none"}`}
