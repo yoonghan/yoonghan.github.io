b377e1d2bf766a92b6719bf158dea973b99ea873
Yoong Han <walcoorperation@gmail.com>
2025-05-21 06:22:10 +0800

    Fix sonarqube test coverage (#424)
    * Fix sonarqube test coverage
    
    * Fix formatting
    
    * Fix formatting

---
+++ src/app/page.jest.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react"
+import Index from "./page"
+
+describe("main page", () => {
+  it("should render main page", () => {
+    render(<Index />)
+    expect(
+      screen.getByRole("heading", { name: "PORTFOLIO PORTAL" })
+    ).toBeInTheDocument()
+  })
+})
