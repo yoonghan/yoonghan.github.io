f89aeec81f9e5cd8cd3aa98c5a73bf709644cdbb
yoonghan <b_wars@yahoo.com>
2025-05-21 06:42:38 +0800

    Fix sync issue for main page test.

--- src/app/page.jest.test.tsx
+++ src/app/page.jest.test.tsx
@@ -2,10 +2,11 @@ import { render, screen } from "@testing-library/react"
 import Index from "./page"
 
 describe("main page", () => {
-  it("should render main page", () => {
+  it("should render main page", async () => {
     render(<Index />)
     expect(
       screen.getByRole("heading", { name: "PORTFOLIO PORTAL" })
     ).toBeInTheDocument()
+    expect(await screen.findByRole("link", { name: "git" })).toBeInTheDocument()
   })
 })
