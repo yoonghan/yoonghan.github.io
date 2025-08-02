1cc0412859c8f9c96bc4f3154569b246d6421bf6
yoonghan <b_wars@yahoo.com>
2025-08-02 08:54:24 +0800

    Revert eslint to 8 and fix linting.

--- src/app/projects/learning/config/NpmCommands.tsx
+++ src/app/projects/learning/config/NpmCommands.tsx
@@ -5,13 +5,19 @@ import { Accordion } from "@yoonghan/walcron-microfrontend-shared"
 const model = [
   {
     label: "Update",
-    content:
-    <ul>
-      <li>npm install "packages@version" # works for even override, update to specific version</li>
-      <li>npm update "packages" # update to latest but based on ~, ^</li>
-      <li>npm audit fix # fix security and may break</li>
-      <li>npm ls "package" # list dependency</li>
-    </ul>
+    content: (
+      <ul>
+        <li>
+          npm install &quot;packages@version&quot; # works for even override,
+          update to specific version
+        </li>
+        <li>
+          npm update &quot;packages&quot; # update to latest but based on ~, ^
+        </li>
+        <li>npm audit fix # fix security and may break</li>
+        <li>npm ls &quot;package&quot; # list dependency</li>
+      </ul>
+    ),
   },
 ]
 
