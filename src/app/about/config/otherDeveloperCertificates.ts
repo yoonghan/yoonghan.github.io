0c7c3229a47c1710d7c8c8f3b10b867cca71491e
yoonghan <b_wars@yahoo.com>
2025-09-07 01:34:40 +0800

    refactor(about): categorize certificates into separate files

---
+++ src/app/about/config/otherDeveloperCertificates.ts
@@ -0,0 +1,26 @@
+import { Certificate } from "@/components/Grid"
+
+export const otherDeveloperCertificates: Certificate[] = [
+  {
+    label: "Functional Programming Principal In Scala",
+    imageSrc: "/favicon.ico", // Placeholder
+    text: "Functional programming with compose, synthentic sugar and immutability.",
+    href: "https://www.coursera.org/account/accomplishments/verify/8CPTGHDQS6",
+  },
+  {
+    label: "Sun Java Certified",
+    imageSrc: "/favicon.ico", // Placeholder
+    text: "Certification on Java programmer.",
+  },
+  {
+    label: "Webassembly with RUST programming",
+    imageSrc: "/favicon.ico", // Placeholder
+    text: "Rusty but worky on RUST programming with javascript.",
+  },
+  {
+    label: "Samurai Leadership",
+    imageSrc: "/favicon.ico", // Placeholder
+    text: "Samurai Leadership - Green Belt.",
+    href: "/certs/samurai-leadership-yoong-han.pdf",
+  },
+]
