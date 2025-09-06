41c972fc635da4da2c3aea38815490264ee5b5c4
yoonghan <b_wars@yahoo.com>
2025-09-07 01:04:51 +0800

    feat: improve certificate grid and consolidate configuration

---
+++ src/app/about/config/certificates.ts
@@ -0,0 +1,104 @@
+import { Certificate } from "@/components/Grid"
+
+export const allCertificates: Certificate[] = [
+  // Cloud Certificates
+  {
+    label: "Microsoft Certified: Azure Solutions Architect Expert",
+    imageSrc: "/img/certifications/microsoft-certified-expert-badge.svg",
+    text: "Azure Certification",
+    href: "https://learn.microsoft.com/en-us/users/hanyoong-5994/credentials/8dbc518645d2a56",
+  },
+  {
+    label: "Microsoft Certified: DevOps Engineer Expert",
+    imageSrc: "/img/certifications/microsoft-certified-expert-badge.svg",
+    text: "Azure Certification",
+    href: "https://learn.microsoft.com/en-us/users/hanyoong-5994/credentials/4b0bb693435b43f0",
+  },
+  {
+    label: "AWS Solution Architect Associate",
+    imageSrc:
+      "https://d1.awsstatic.com/certification/badges/AWS-Certified-Solutions-Architect-Associate_badge_150x150.e359ae4a6d4d82c3e31d4f9104c8d389b56a2423.png",
+    text: "Aws Certification",
+    href: "https://www.credly.com/badges/964f2164-02ab-469c-b964-a01c2210ef24",
+  },
+  {
+    label: "Azure Administrator Associate",
+    imageSrc: "/img/certifications/microsoft-certified-associate-badge.svg",
+    text: "Azure Certification",
+    href: "https://learn.microsoft.com/api/credentials/share/en-us/HanYoong-5994/AE5F2EC02868991B",
+  },
+  {
+    label: "AWS Developer Associate",
+    imageSrc:
+      "https://d1.awsstatic.com/training-and-certification/certification-badges/AWS-Certified-Developer-Associate_badge.5c083fa855fe82c1cf2d0c8b883c265ec72a17c0.png",
+    text: "Aws Certification",
+    href: "https://www.credly.com/badges/fb17e291-5f18-4c0f-9474-364d82549bbe",
+  },
+  {
+    label: "Azure Cloud Fundamentals (AZ-900)",
+    imageSrc: "/img/certifications/microsoft-certified-fundamentals-badge.svg",
+    text: "Azure Certification",
+    href: "https://learn.microsoft.com/api/credentials/share/en-us/HanYoong-5994/47B43AF13DE84FB9",
+  },
+  {
+    label: "AWS Cloud Practitioner",
+    imageSrc:
+      "https://d1.awsstatic.com/certification/badges/AWS-Certified-Cloud-Practitioner_badge_150x150.17da917fbddc5383838d9f8209d2030c8d99f31e.png",
+    text: "Aws Certification",
+    href: "https://www.credly.com/badges/b17e3872-c5c5-49f7-8c6a-186e107a28d2",
+  },
+  // AI Certificates
+  {
+    label: "AWS Certified AI Practitioner",
+    imageSrc:
+      "https://images.credly.com/size/680x680/images/4d4693bb-530e-4bca-9327-de07f3aa2348/image.png",
+    text: "Aws Certification",
+    href: "https://www.credly.com/badges/706cb51a-ba27-4587-ae20-8dca06007bc4",
+  },
+  {
+    label: "Google Prompting Essentials",
+    imageSrc:
+      "https://coursera-university-assets.s3.amazonaws.com/4a/cb36835ae3421187080898a7ecc11d/Google-G_360x360.png?auto=format%2Ccompress&dpr=2&w=60&h=60",
+    text: "Recognize and apply the prompting framework to create effective prompts.",
+    href: "https://coursera.org/share/d0dae1978ec539fe9dfa36acae5dfce8",
+  },
+  {
+    label: "Introduction to Github Copilot",
+    imageSrc:
+      "https://coursera-university-assets.s3.amazonaws.com/73/8792b24ae547c1a582c33f7ea4d569/Microsoft-logo_rgb_200x43.png?auto=format%2Ccompress&dpr=2&h=45",
+    text: "Use Gitlab Co-pilot for Python programming.",
+    href: "https://www.coursera.org/account/accomplishments/verify/ATLFGOBTUICD",
+  },
+  {
+    label: "Effective ChatGPT",
+    imageSrc:
+      "https://images.credly.com/images/bfbfeb07-49b9-4cce-866c-f06ef9e2aae9/f9681bc8-450d-40fd-81c4-a3a20e483c0b.png",
+    text: "Learning ChatGPT. [Foundation]",
+    href: "https://www.credly.com/badges/395a3138-11d3-4144-94dd-2389c52213aa",
+  },
+  // Programming Certificates
+  {
+    label: "Functional Programming Principal In Scala",
+    imageSrc: "/favicon.ico", // Placeholder
+    text: "Functional programming with compose, synthentic sugar and immutability.",
+    href: "https://www.coursera.org/account/accomplishments/verify/8CPTGHDQS6",
+  },
+  {
+    label: "Sun Java Certified",
+    imageSrc: "/favicon.ico", // Placeholder
+    text: "Certification on Java programmer.",
+    href: "#", // No link provided
+  },
+  {
+    label: "Webassembly with RUST programming",
+    imageSrc: "/favicon.ico", // Placeholder
+    text: "Rusty but worky on RUST programming with javascript.",
+    href: "#", // No link provided
+  },
+  {
+    label: "Samurai Leadership",
+    imageSrc: "/favicon.ico", // Placeholder
+    text: "Samurai Leadership - Green Belt.",
+    href: "/certs/samurai-leadership-yoong-han.pdf",
+  },
+]
