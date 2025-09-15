a14b23da9fd298226106b977022d0109d172875b
yoonghan <b_wars@yahoo.com>
2025-09-16 07:25:31 +0800

    Add error instance util.

---
+++ src/util/errorHandler.ts
@@ -0,0 +1,12 @@
+export const toError = (error: unknown): Error => {
+  if (error instanceof Error) {
+    return error
+  }
+  if (typeof error === "string") {
+    return new Error(error)
+  }
+  if (typeof error === "object" && error !== null) {
+    return new Error(JSON.stringify(error))
+  }
+  return new Error(String(error))
+}
