7b60c02c1cbde41989f8614a20b0115da4b6ac8f
yoonghan <b_wars@yahoo.com>
2025-09-20 06:03:21 +0800

    Add otel-web with service name.

--- src/util/otel-web.ts
+++ src/util/otel-web.ts
@@ -7,6 +7,7 @@ import { registerInstrumentations } from "@opentelemetry/instrumentation"
 import { FetchInstrumentation } from "@opentelemetry/instrumentation-fetch"
 import { ZoneContextManager } from "@opentelemetry/context-zone"
 import { site } from "@/config/site"
+import { Resource, RawResourceAttribute } from "@opentelemetry/resources"
 
 export const initOpenTelemetry = (window: Window | undefined) => {
   if (typeof window !== "undefined") {
@@ -17,6 +18,17 @@ export const initOpenTelemetry = (window: Window | undefined) => {
     )
 
     const provider = new WebTracerProvider({
+      resource: {
+        attributes: {
+          "service-name": "web",
+        },
+        merge: function (other: Resource | null): Resource {
+          throw new Error("Function not implemented.")
+        },
+        getRawAttributes: function (): RawResourceAttribute[] {
+          throw new Error("Function not implemented.")
+        },
+      },
       spanProcessors: [spanProcessor],
     })
 
