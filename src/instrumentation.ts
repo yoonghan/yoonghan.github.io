5b584a8acdb0285b5a73e2840738903ad39e5e02
yoonghan <b_wars@yahoo.com>
2025-11-01 01:03:07 +0800

    Upgrade to latest nextjs.

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,26 +1,26 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-export function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { NodeSDK } = require("@opentelemetry/sdk-node")
-    const {
-      getNodeAutoInstrumentations,
-    } = require("@opentelemetry/auto-instrumentations-node")
-    const {
-      OTLPTraceExporter,
-    } = require("@opentelemetry/exporter-trace-otlp-http")
+// /* eslint-disable @typescript-eslint/no-require-imports */
+// export function register() {
+//   if (process.env.NEXT_RUNTIME === "nodejs") {
+//     const { NodeSDK } = require("@opentelemetry/sdk-node")
+//     const {
+//       getNodeAutoInstrumentations,
+//     } = require("@opentelemetry/auto-instrumentations-node")
+//     const {
+//       OTLPTraceExporter,
+//     } = require("@opentelemetry/exporter-trace-otlp-http")
 
-    const sdk = new NodeSDK({
-      serviceName: "api",
-      traceExporter: new OTLPTraceExporter({
-        url: "https://api.axiom.co/v1/traces",
-        headers: {
-          Authorization: `Bearer ${process.env.AXIOM_API_TOKEN}`,
-          "X-Axiom-Dataset": `${process.env.AXIOM_DATASET_NAME}`,
-        },
-      }),
-      instrumentations: [getNodeAutoInstrumentations()],
-    })
+//     const sdk = new NodeSDK({
+//       serviceName: "api",
+//       traceExporter: new OTLPTraceExporter({
+//         url: "https://api.axiom.co/v1/traces",
+//         headers: {
+//           Authorization: `Bearer ${process.env.AXIOM_API_TOKEN}`,
+//           "X-Axiom-Dataset": `${process.env.AXIOM_DATASET_NAME}`,
+//         },
+//       }),
+//       instrumentations: [getNodeAutoInstrumentations()],
+//     })
 
-    sdk.start()
-  }
-}
+//     sdk.start()
+//   }
+// }
