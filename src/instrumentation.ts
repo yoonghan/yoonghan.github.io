75dd05c331cde79826b63b6d357cbf4858a42181
yoonghan <b_wars@yahoo.com>
2025-11-01 07:44:24 +0800

    Refactor to use import with nodejs.

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,26 +1,21 @@
-// /* eslint-disable @typescript-eslint/no-require-imports */
-// export function register() {
-//   if (process.env.NEXT_RUNTIME === "nodejs") {
-//     const { NodeSDK } = require("@opentelemetry/sdk-node")
-//     const {
-//       getNodeAutoInstrumentations,
-//     } = require("@opentelemetry/auto-instrumentations-node")
-//     const {
-//       OTLPTraceExporter,
-//     } = require("@opentelemetry/exporter-trace-otlp-http")
+import { NodeSDK } from "@opentelemetry/sdk-node"
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node"
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http"
 
-//     const sdk = new NodeSDK({
-//       serviceName: "api",
-//       traceExporter: new OTLPTraceExporter({
-//         url: "https://api.axiom.co/v1/traces",
-//         headers: {
-//           Authorization: `Bearer ${process.env.AXIOM_API_TOKEN}`,
-//           "X-Axiom-Dataset": `${process.env.AXIOM_DATASET_NAME}`,
-//         },
-//       }),
-//       instrumentations: [getNodeAutoInstrumentations()],
-//     })
+export function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    const sdk = new NodeSDK({
+      serviceName: "api",
+      traceExporter: new OTLPTraceExporter({
+        url: "https://api.axiom.co/v1/traces",
+        headers: {
+          Authorization: `Bearer ${process.env.AXIOM_API_TOKEN}`,
+          "X-Axiom-Dataset": `${process.env.AXIOM_DATASET_NAME}`,
+        },
+      }),
+      instrumentations: [getNodeAutoInstrumentations()],
+    })
 
-//     sdk.start()
-//   }
-// }
+    sdk.start()
+  }
+}
