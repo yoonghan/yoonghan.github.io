25a3f4650e59e6dba28ffddd66bf3ccddfedaea3
yoonghan <b_wars@yahoo.com>
2025-11-06 06:40:00 +0800

    Restore sitemap

---
+++ src/app/sitemap.test.ts
@@ -0,0 +1,17 @@
+import "@/__tests__/mocks/site"
+import sitemapGenerator, { dynamic } from "./sitemap"
+import "@/__tests__/mocks/fetchMock"
+
+describe("sitemap", () => {
+  it("should generate there right body", () => {
+    const response = sitemapGenerator()
+    expect(response[0].url).toBe("https://mockedUrl.com/")
+    expect(response[0].lastModified).not.toBe(undefined)
+    expect(response[1].url).toBe("https://mockedUrl.com/about")
+    expect(response[1].lastModified).not.toBe(undefined)
+  })
+
+  it("export dynamic routes", () => {
+    expect(dynamic).toBe("force-static")
+  })
+})
