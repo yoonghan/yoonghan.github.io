95a653a093f585e2fe96d7bc6898591c462b0605
yoonghan <b_wars@yahoo.com>
2026-03-01 08:08:05 +0800

    Update eslint with latest changes.

--- src/app/projects/certifications/config/AzureCosmosDBDeveloperSpecialty.tsx
+++ src/app/projects/certifications/config/AzureCosmosDBDeveloperSpecialty.tsx
@@ -3,14 +3,25 @@
 import Link from "@/components/Link"
 
 export function AzureCosmosDBDeveloperSpecialty() {
-    return (
-        <article>
-            <p className="text-2xl">Microsoft Certified: Azure Cosmos DB Developer Specialty(DP-420)</p>
-            <p>
-                In-depth knowledge of Azure Cosmos DB and how to use it in real world scenarios. Very useful for developers to design NoSQL solutions.
-                This exam's fun place is that most resources can be obtain in 1 umbrella link which is <Link href="https://learn.microsoft.com/en-us/azure/cosmos-db/overview">Azure Cosmos DB Overview</Link>.
-            </p>
-            <strong>Notes:</strong> <Link href="https://github.com/users/yoonghan/projects/9/views/1">Azure Cosmos DB Developer Specialty</Link>
-        </article>
-    )
+  return (
+    <article>
+      <p className="text-2xl">
+        Microsoft Certified: Azure Cosmos DB Developer Specialty(DP-420)
+      </p>
+      <p>
+        In-depth knowledge of Azure Cosmos DB and how to use it in real world
+        scenarios. Very useful for developers to design NoSQL solutions. This
+        exam&apos;s fun place is that most resources can be obtain in 1 umbrella
+        link which is{" "}
+        <Link href="https://learn.microsoft.com/en-us/azure/cosmos-db/overview">
+          Azure Cosmos DB Overview
+        </Link>
+        .
+      </p>
+      <strong>Notes:</strong>{" "}
+      <Link href="https://github.com/users/yoonghan/projects/9/views/1">
+        Azure Cosmos DB Developer Specialty
+      </Link>
+    </article>
+  )
 }
