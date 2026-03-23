bf96bd2e0f3b8045c42c1e5015d0f21068e15545
yoonghan <b_wars@yahoo.com>
2026-03-24 07:18:37 +0800

    Remove PWA

--- src/app/projects/checklist/Checklist.test.tsx
+++ src/app/projects/checklist/Checklist.test.tsx
@@ -5,7 +5,7 @@ import {
   screen,
   getDefaultNormalizer,
 } from "@testing-library/react"
-import { CronJobCheckList, TroubleshootPwaCheckList } from "./Checklist"
+import { CronJobCheckList } from "./Checklist"
 import { setServiceNavigator } from "@/__tests__/mocks/windowMock"
 import userEvent from "@testing-library/user-event"
 import { fetchMock } from "@/__tests__/mocks/fetchMock"
@@ -90,43 +90,5 @@ describe("Checklist", () => {
     })
   })
 
-  describe("TroubleshootPwaCheckList", () => {
-    const assertValue = (component: HTMLElement, value: boolean) => {
-      // eslint-disable-next-line testing-library/no-node-access
-      const trElem = component.parentElement
-      expect(trElem).not.toBeNull
-      if (trElem !== null)
-        expect(within(trElem).getByText(value ? "true" : "false"))
-    }
 
-    it("should render correctly", () => {
-      render(<TroubleshootPwaCheckList />)
-      expect(screen.getByText("PWA Registered")).toBeInTheDocument()
-      expect(
-        screen.getByText("Trusted Site App (N/R, query string issue)"),
-      ).toBeInTheDocument()
-      expect(screen.getByText("Detected New Update (N/T)")).toBeInTheDocument()
-      expect(screen.getByText("Update Installed (N/T)")).toBeInTheDocument()
-      expect(screen.getByText("PWA run offline")).toBeInTheDocument()
-    })
-
-    it("should be able to set all service to true, except for twa", async () => {
-      setServiceNavigator()
-      render(<TroubleshootPwaCheckList />)
-      expect(await screen.findAllByText("true")).toHaveLength(3)
-      assertValue(screen.getByText("PWA Registered"), true)
-      assertValue(screen.getByText("Detected New Update (N/T)"), true)
-      assertValue(screen.getByText("Update Installed (N/T)"), true)
-    })
-
-    xit("should be able to set twa to true", async () => {
-      window.location.href = "?utm_source=launcher"
-      render(<TroubleshootPwaCheckList />)
-      expect(await screen.findAllByText("true")).toHaveLength(1)
-      assertValue(
-        screen.getByText("Trusted Site App (N/R, query string issue)"),
-        true,
-      )
-    })
-  })
 })
