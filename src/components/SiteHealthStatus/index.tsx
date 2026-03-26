09bb94b22a2bff5eebf4aa3f765f0e049fb59ddc
yoonghan <mailyoonghan@gmail.com>
2026-03-26 17:21:25 +0800

    Refactor and also remove h3

--- src/components/SiteHealthStatus/index.tsx
+++ src/components/SiteHealthStatus/index.tsx
@@ -7,19 +7,20 @@ export interface SiteHealthStatusProps {
 
 function SiteHealthStatus({ repo, codecovToken }: SiteHealthStatusProps) {
     const sonarId = repo.replace("/", "_")
+    const badgeClass = "inline border-0 mr-2 mb-2"
 
     return (
         <article>
-            <h3 className="text-3xl text-center pb-8" id="status">
+            <div className="text-3xl text-center pb-8" id="status">
                 Site Health Status
-            </h3>
+            </div>
             <p className="text-justify whitespace-normal break-words">
                 <Link href={`https://github.com/${repo}/actions/workflows/merge.yml`}>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                         src={`https://github.com/${repo}/actions/workflows/merge.yml/badge.svg`}
                         alt="Report merged result on main branch"
-                        className="inline border-0 mr-2 mb-2"
+                        className={badgeClass}
                     />
                 </Link>
                 <Link href={`https://codecov.io/gh/${repo}`}>
@@ -27,7 +28,7 @@ function SiteHealthStatus({ repo, codecovToken }: SiteHealthStatusProps) {
                     <img
                         src={`https://codecov.io/gh/${repo}/graph/badge.svg?token=${codecovToken}`}
                         alt="Code coverage"
-                        className="inline border-0 mr-2 mb-2"
+                        className={badgeClass}
                     />
                 </Link>
                 <Link href={`https://sonarcloud.io/summary/new_code?id=${sonarId}`}>
@@ -35,7 +36,7 @@ function SiteHealthStatus({ repo, codecovToken }: SiteHealthStatusProps) {
                     <img
                         src={`https://sonarcloud.io/api/project_badges/measure?project=${sonarId}&metric=bugs`}
                         alt="Bugs"
-                        className="inline border-0 mr-2 mb-2"
+                        className={badgeClass}
                     />
                 </Link>
                 <Link href={`https://sonarcloud.io/summary/new_code?id=${sonarId}`}>
@@ -43,7 +44,7 @@ function SiteHealthStatus({ repo, codecovToken }: SiteHealthStatusProps) {
                     <img
                         src={`https://sonarcloud.io/api/project_badges/measure?project=${sonarId}&metric=code_smells`}
                         alt="Code Smells"
-                        className="inline border-0 mr-2 mb-2"
+                        className={badgeClass}
                     />
                 </Link>
                 <Link href={`https://sonarcloud.io/summary/new_code?id=${sonarId}`}>
@@ -51,7 +52,7 @@ function SiteHealthStatus({ repo, codecovToken }: SiteHealthStatusProps) {
                     <img
                         src={`https://sonarcloud.io/api/project_badges/measure?project=${sonarId}&metric=vulnerabilities`}
                         alt="Vulnerabilities"
-                        className="inline border-0 mr-2 mb-2"
+                        className={badgeClass}
                     />
                 </Link>
                 <Link href={`https://sonarcloud.io/summary/new_code?id=${sonarId}`}>
@@ -59,7 +60,7 @@ function SiteHealthStatus({ repo, codecovToken }: SiteHealthStatusProps) {
                     <img
                         src={`https://sonarcloud.io/api/project_badges/measure?project=${sonarId}&metric=security_rating`}
                         alt="Security Rating"
-                        className="inline border-0 mr-2 mb-2"
+                        className={badgeClass}
                     />
                 </Link>
             </p>
