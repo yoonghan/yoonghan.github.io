62d741fecab735ffad7431f2f93f04e6cdc59ef1
yoonghan <b_wars@yahoo.com>
2026-04-02 07:36:23 +0800

    Add code line coverage.

--- src/util/location.test.ts
+++ src/util/location.test.ts
@@ -11,7 +11,7 @@ describe("location", () => {
         consoleSpy.mockRestore()
     })
 
-    it("should run basic browser location", () => {
+    it("should run basic location function existance which should not be mocked", () => {
         expect(search()).toBe("")
 
         expect(redirectTo("http://www.walcron.com")).toBeUndefined()
