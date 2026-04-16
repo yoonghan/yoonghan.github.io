8b51acbd601dbb3e9cee7c8cbbc78f4a49413044
yoonghan <b_wars@yahoo.com>
2026-04-16 12:33:42 +0800

    Generalize Loader as component.

---
+++ src/components/TextLoader/TextLoader.tsx
@@ -0,0 +1,14 @@
+"use client"
+import styles from "./TextLoader.module.css"
+
+const TextLoader = ({ text }: { text: string }) => {
+    return (
+        <div>
+            <div className={`text-center my-16 italic text-green-700 q${styles.dots}`}>
+                {text}<span>.</span><span>.</span><span>.</span>
+            </div>
+        </div>
+    )
+}
+
+export default TextLoader
