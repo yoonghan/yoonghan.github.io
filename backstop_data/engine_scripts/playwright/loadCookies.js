7917eb998333688c2a036b1032b19d69d2e36d03
han <b_wars@yahoo.com>
2026-04-25 17:32:43 +0800

    chore: restore reference snapshots after history purge

---
+++ backstop_data/engine_scripts/playwright/loadCookies.js
@@ -0,0 +1,20 @@
+const fs = require("node:fs")
+
+module.exports = async (browserContext, scenario) => {
+	let cookies = []
+	const cookiePath = scenario.cookiePath
+	const cookieDomain = scenario.cookieDomain
+
+	// Read Cookies from File, if exists
+	if (cookiePath && fs.existsSync(cookiePath)) {
+		cookies = JSON.parse(fs.readFileSync(cookiePath))
+	}
+
+	// Replace with cookie with domain
+	cookies = cookies.map((cookie) => ({ ...cookie, domain: cookieDomain }))
+
+	// Add cookies to browser
+	browserContext.addCookies(cookies)
+
+	console.log("Cookie state restored with:", JSON.stringify(cookies, null, 2))
+}
